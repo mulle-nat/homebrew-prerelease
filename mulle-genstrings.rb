e7e7811e2f95197db2bc53e19c1b67e822d83a62
Nat! <nat@mulle-kybernetik.com>
2015-10-10 15:13:24 +0200

    v18.48.4 released

--- mulle-genstrings.rb
+++ mulle-genstrings.rb
@@ -1,9 +1,9 @@
 class MulleGenstrings < Formula
   homepage "http://www.mulle-kybernetik.com/software/git/mulle-genstrings"
   desc "a replacement for Apple's genstrings"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-genstrings/tarball/18.48.3"
-  version "18.48.3"
-  sha256 "7a846e8b8ec35b971d2df4204a52328d3664285c6a551fb602e9c6219150e7ba"
+  url "http://www.mulle-kybernetik.com/software/git/mulle-genstrings/tarball/18.48.4"
+  version "18.48.4"
+  sha256 "6e2c82d9e3e707882fa626631de0afe87cefc34f50e7b5c79f41b1d2c01a5001"
 
   depends_on :xcode => :build
   depends_on :macos => :snow_leopard
