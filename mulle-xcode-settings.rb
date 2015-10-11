3014df907572239ce8907df940e962a8d6b63293
Nat! <nat@mulle-kybernetik.com>
2015-10-11 13:37:42 +0200

    1.0.3

--- mulle-xcode-settings.rb
+++ mulle-xcode-settings.rb
@@ -1,9 +1,9 @@
 class MulleXcodeSettings < Formula
   homepage "http://www.mulle-kybernetik.com/software/git/mulle-xcode-settings"
   desc "change Xcode project build settings from the command line"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-xcode-settings/tarball/1.0.2"
-  version "1.0.2"
-  sha256 "fd2893d976483d0c2ec11a1fd632b2315832561c4355e6218ec337d4c9428761"
+  url "http://www.mulle-kybernetik.com/software/git/mulle-xcode-settings/tarball/1.0.3"
+  version "1.0.3"
+  sha256 "335939a4ac749318bdcee5a9fbc0fa8fcadf2212b8bea1556f74d48906a19c3d"
 
   depends_on :xcode => :build
   depends_on :macos => :snow_leopard
