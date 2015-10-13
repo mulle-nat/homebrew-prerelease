a4816f7af23c333f5d1ba347a7c7e1afe8576e1a
Nat! <nat@mulle-kybernetik.com>
2015-10-13 15:39:26 +0200

    0.8 release

--- mulle-bootstrap.rb
+++ mulle-bootstrap.rb
@@ -1,9 +1,9 @@
 class MulleBootstrap < Formula
   homepage "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap"
   desc "A simple shell script based dependency manager for projects"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap/tarball/0.7.1"
-  version "0.7.1"
-  sha256 "442bbb74ade29103d2fe20cf7da6bd49ae5743919a2e192ead0f0a40baa8273c"
+  url "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap/tarball/0.8"
+  version "0.8"
+  sha256 "12f426101a4b41aa1d1781e8cb252ac4756cc27aa77473448f66c89862f7c94e"
 
   def install
      system "./install.sh", "#{prefix}"
