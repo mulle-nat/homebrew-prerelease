458efe57b138267eff6b1e45bc5f6d4adea7f68b
Nat! <nat@codeon.de>
2016-02-29 14:16:37 +0100

    fix

--- mulle-xcode-upgrade-fix.rb
+++ mulle-xcode-upgrade-fix.rb
@@ -1,15 +1,15 @@
-class MulleBootstrap < Formula
-  homepage "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap"
+class mulle-xcode-upgrade-fix < Formula
+  homepage "https://www.mulle-kybernetik.com/software/git/mulle-xcode-upgrade-fix"
   desc "Fix unwanted upgrade warnings in Xcode projects"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap/tarball/0.2"
+  url "https://www.mulle-kybernetik.com/software/git/mulle-xcode-upgrade-fix/tarball/0.2"
   version "0.2"
-  sha256 "7885789cc30e23732f626d5e47394aacca800477844aa090c0e36214da95fcd4"
+  sha256 "2f680047407ae89450d210b303f721b42000cf555a95834f284ba1f9cf515d25"
 
   def install
-     system "install", "-m", "0775", "mulle-xcode-upgrade-fix.sh" "#{prefix}"
+     system "install", "-m", "0775", "mulle-xcode-upgrade-fix.sh", "#{prefix}"
   end
 
   test do
   end
 end
-# FORMULA mulle-bootstrap.rb
+# FORMULA mulle-xcode-upgrade-fix.rb
