daf5f8bf1e1a8e83ac6b5370cb2bb1d0a2ceefb1
Nat! <nat@mulle-kybernetik.com>
2016-09-22 21:44:47 +0200

    3.0.0 release of mulle-thread

--- mulle-thread.rb
+++ mulle-thread.rb
@@ -3,7 +3,10 @@ class MulleThread < Formula
   desc "Cross-platform threads and atomic operations"
   url "http://www.mulle-kybernetik.com/software/git/mulle-thread/tarball/3.0.0"
   version "3.0.0"
-  sha256 "20ae7b059a9b948a10b52a5abd014c2bc1f544028b39db0893666cbc3227eb36"
+  sha256 "3bf709448f375f589f97038c565661bfead5617e76e530172658532e6626a59c"
+
+  depends_on 'cmake'           => :build
+  depends_on 'mulle-bootstrap' => :build
 
   def install
      system "./install.sh", "#{prefix}"
