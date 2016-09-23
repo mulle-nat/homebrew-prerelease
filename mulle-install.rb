b5cd1afbe546a5451976068713ed879556428b9b
Nat! <nat@codeon.de>
2016-09-23 16:04:32 +0200

    0.1 release of mulle-install

--- mulle-install.rb
+++ mulle-install.rb
@@ -3,11 +3,10 @@ class MulleInstall < Formula
   desc "Cross-platform installer using mulle-bootstrap or cmake"
   url "http://www.mulle-kybernetik.com/software/git/mulle-install/tarball/0.1"
   version "0.1"
-  sha256 "0cc31ec40697f67592a22504896d450defcaa6500d33c4f5233ae2c12410f405"
+  sha256 "ad0495671dc18022ada7c03dd9300bed4dcf2636f4a3d120dd7a6292eb0befb5"
 
   depends_on 'mulle-bootstrap'
   depends_on 'cmake'
-  depends_on 'make'
 
   def install
      system "./install.sh", "#{prefix}"
