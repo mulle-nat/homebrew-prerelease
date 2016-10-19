dd0f651f664a8d313004eb82fdb35f6b9106a31e
Nat! <nat@codeon.de>
2016-10-19 12:32:40 +0200

    1.0.2 release of mulle-concurrent

--- mulle-concurrent.rb
+++ mulle-concurrent.rb
@@ -1,9 +1,9 @@
 class MulleConcurrent < Formula
    homepage "https://www.mulle-kybernetik.com/software/git/mulle-concurrent"
    desc "Lock- and Wait-free Hashtable (and an Array/ too) in C"
-   url "https://www.mulle-kybernetik.com/software/git/mulle-concurrent/tarball/1.0.1"
-   version "1.0.1"
-   sha256 "b25dc2939360582f30b2b650eed4076a7ee81224d354a0424499ea5b2fda6eef"
+   url "https://www.mulle-kybernetik.com/software/git/mulle-concurrent/tarball/1.0.2"
+   version "1.0.2"
+   sha256 "e191d3e60236f80d600f466bd8fb5a0d8e5c06112c036f1d8fa1177a52b7e73f"
 
    depends_on 'mulle-c11'
    depends_on 'mulle-allocator'
