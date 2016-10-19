2ca57d5be70a444d1fefe1cfcdad92da774ae983
Nat! <nat@codeon.de>
2016-10-19 14:22:23 +0200

    1.0.2 release of mulle-c11

--- mulle-c11.rb
+++ mulle-c11.rb
@@ -1,9 +1,9 @@
 class MulleC11 < Formula
   homepage "http://www.mulle-kybernetik.com/software/git/mulle-c11"
   desc "Cross-platform compiler glue"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-c11/tarball/1.0.1"
-  version "1.0.1"
-  sha256 "332569218484bc872f834547a6f8d68de02c7664ebb67d5012e720bce9a5654e"
+  url "http://www.mulle-kybernetik.com/software/git/mulle-c11/tarball/1.0.2"
+  version "1.0.2"
+  sha256 "b0a44f829512f3727c27eb80b59fb6bafa3f3365838e11ba01ff75abe4d66cb6"
 
   depends_on 'mulle-bootstrap' => :build
   depends_on 'mulle-build' => :build
