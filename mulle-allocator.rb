ad4bcbf32693e2a87d132fb24bb86ff72b754f20
Nat! <nat@mulle-kybernetik.com>
2016-11-08 14:52:37 +0100

    2.1.1 release of mulle-allocator

--- mulle-allocator.rb
+++ mulle-allocator.rb
@@ -1,12 +1,12 @@
 class MulleAllocator < Formula
-   homepage "https://www.mulle-kybernetik.com/software/git/mulle-allocator"
+   homepage "https://github.com/mulle-nat/mulle-allocator"
    desc "Flexible C memory allocation scheme with leak checking"
-   url "https://www.mulle-kybernetik.com/software/git/mulle-allocator/tarball/2.0.0"
-   version "2.0.0"
-   sha256 "8f46b97ff56cea53f17b9e9a3ce7f17b3b5627686ba013f732706a5d2b7f3002"
+   url "https://github.com/mulle-nat/mulle-allocator/archive/2.1.1.tar.gz"
+   version "2.1.1"
+   sha256 "e20075a393aa31a6407f944581d54a7a0b0e13c5065cbb96196150b31071b417"
 
-   depends_on 'mulle-c11'
-   depends_on 'mulle-thread'
+   depends_on 'mulle-nat/software/mulle-c11'
+   depends_on 'mulle-nat/software/mulle-thread'
    depends_on 'mulle-build' => :build
 
    def install
