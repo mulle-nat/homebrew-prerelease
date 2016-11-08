ffa0d442ed0a1d0bfccf848398621337ed0e502d
Nat! <nat@mulle-kybernetik.com>
2016-11-08 15:43:37 +0100

    1.1.3 release of mulle-concurrent

--- mulle-concurrent.rb
+++ mulle-concurrent.rb
@@ -1,15 +1,15 @@
 class MulleConcurrent < Formula
    homepage "https://github.com/mulle-nat/mulle-concurrent"
    desc "Lock- and Wait-free Hashtable (and an Array too) in C"
-   url "https://github.com/mulle-nat/mulle-concurrent/archive/1.1.3.tar.gz"
+   url "https://github.com/mulle-kybernetik/mulle-concurrent/archive/1.1.3.tar.gz"
    version "1.1.3"
-   sha256 "af942e7b09e3437c472d2c79c8d006b1dd31f1b15f424bd0bc23509ab63a7fff"
+   sha256 "e28bf7d9fcc22cdfde0c7c8f31e3648a4847e7bda9cb69f309f24257eee3dd41"
 
-   depends_on 'mulle-nat/software/mulle-c11'
-   depends_on 'mulle-nat/software/mulle-allocator'
-   depends_on 'mulle-nat/software/mulle-aba'
-   depends_on 'mulle-nat/software/mulle-thread'
-   depends_on 'mulle-build' => :build
+   depends_on 'mulle-kybernetik/software/mulle-c11'
+   depends_on 'mulle-kybernetik/software/mulle-allocator'
+   depends_on 'mulle-kybernetik/software/mulle-aba'
+   depends_on 'mulle-kybernetik/software/mulle-thread'
+   depends_on 'mulle-kybernetik/software/mulle-build' => :build
 
    def install
       system "mulle-install", "-e", "--prefix", "#{prefix}"
