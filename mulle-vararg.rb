4bd92eb959092e90cab3347716ccf45b6cdae79f
Nat! <nat@mulle-kybernetik.com>
2016-11-08 14:56:50 +0100

    0.5.11 release of mulle-vararg

--- mulle-vararg.rb
+++ mulle-vararg.rb
@@ -1,11 +1,11 @@
 class MulleVararg < Formula
-   homepage "https://www.mulle-kybernetik.com/software/git/mulle-vararg"
+   homepage "https://github.com/mulle-nat/mulle-vararg"
    desc "Access variable arguments in struct layout fashion"
-   url "https://www.mulle-kybernetik.com/software/git/mulle-vararg/tarball/0.5.8"
-   version "0.5.8"
-   sha256 "4296154b174cf74fb7f9b55300bd6e1c41f37f43674941494f3fa0889ebf5f47"
+   url "https://github.com/mulle-nat/mulle-vararg/archive/0.5.11.tar.gz"
+   version "0.5.11"
+   sha256 "df17ff2349e8736f8506022a76ef9b5e1105e07fcbd128dbc531c68d2080a438"
 
-   depends_on 'mulle-c11'
+   depends_on 'mulle-nat/software/mulle-c11'
    depends_on 'mulle-build' => :build
 
    def install
