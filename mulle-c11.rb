f8c462c9394be4a834fb155fef45c5c64cdee4a6
Nat! <nat@mulle-kybernetik.com>
2016-11-08 16:24:58 +0100

    1.0.5 release of mulle-c11

--- mulle-c11.rb
+++ mulle-c11.rb
@@ -1,9 +1,9 @@
 class MulleC11 < Formula
    homepage "https://github.com/mulle-nat/mulle-c11"
    desc "Cross-platform compiler glue"
-   url "https://github.com/mulle-kybernetik/mulle-c11/archive/1.0.5.tar.gz"
+   url "https://github.com/mulle-nat/mulle-c11/archive/1.0.5.tar.gz"
    version "1.0.5"
-   sha256 "e28bf7d9fcc22cdfde0c7c8f31e3648a4847e7bda9cb69f309f24257eee3dd41"
+   sha256 "e7ee6a9b4ca997d4f6835e8171f859279d94d0bd24de9e3a04a2f3581367ce7c"
 
    depends_on 'mulle-kybernetik/software/mulle-build' => :build
 
