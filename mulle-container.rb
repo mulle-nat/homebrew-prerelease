cb614983df5342c0a16023b8ca252524876fccfe
Nat! <nat@mulle-kybernetik.com>
2016-11-08 17:03:48 +0100

    0.8.3 release of mulle-container

--- mulle-container.rb
+++ mulle-container.rb
@@ -1,9 +1,9 @@
 class MulleContainer < Formula
    homepage "https://github.com/mulle-nat/mulle-container"
    desc "Sets, hashtables, byte buffers and more, written in C"
-   url "https://github.com/mulle-kybernetik/mulle-container/archive/0.8.3.tar.gz"
+   url "https://github.com/mulle-nat/mulle-container/archive/0.8.3.tar.gz"
    version "0.8.3"
-   sha256 "e28bf7d9fcc22cdfde0c7c8f31e3648a4847e7bda9cb69f309f24257eee3dd41"
+   sha256 "78490bea2e4440a1315730b3ea920eca9f0d9da589027b5294405725c44ea38a"
 
    depends_on 'mulle-kybernetik/software/mulle-c11'
    depends_on 'mulle-kybernetik/software/mulle-allocator'
