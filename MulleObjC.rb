a52e6d65b70a208141ae7058f9aabc28baa41954
Nat! <nat@mulle-kybernetik.com>
2016-11-10 20:16:12 +0100

    0.1.2 release of MulleObjC

--- MulleObjC.rb
+++ MulleObjC.rb
@@ -1,9 +1,9 @@
 class Mulleobjc < Formula
    homepage "https://github.com/mulle-nat/MulleObjC"
    desc "Objective-C root classes using the mulle-objc runtime"
-   url "https://github.com/mulle-nat/MulleObjC/archive/0.1.1.tar.gz"
-   version "0.1.1"
-   sha256 "cdbf09807330a8606b29d906bdfb981ce310490e125a66c84fed873bf6b6f275"
+   url "https://github.com/mulle-nat/MulleObjC/archive/0.1.2.tar.gz"
+   version "0.1.2"
+   sha256 "daee7a1b859bda14f962b30e02393820b82cab4eb3a27d76b28f5055068f2740"
 
    depends_on 'mulle-kybernetik/software/mulle-objc'
    depends_on 'mulle-kybernetik/software/mulle-container'
