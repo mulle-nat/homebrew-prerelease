7bd4d6a4e30a6eafd5d8ede73da6f45510e7fce5
Nat! <nat@codeon.de>
2016-11-11 19:24:15 +0100

    0.8.2 release of mulle-build

--- mulle-build.rb
+++ mulle-build.rb
@@ -1,9 +1,9 @@
 class MulleBuild < Formula
   homepage "http://www.mulle-kybernetik.com/software/git/mulle-build"
   desc "Cross-platform builder using cmake and mulle-bootstrap"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-build/tarball/0.8.1"
-  version "0.8.1"
-  sha256 "4a9bbe39f975a5b30e9f5554d6c061922d2a95f7662da1245044d66435c0cfe3"
+  url "http://www.mulle-kybernetik.com/software/git/mulle-build/tarball/0.8.2"
+  version "0.8.2"
+  sha256 "56a4b1436ced6a5acf35301df32db76db1e5c2698ab8e1e49d5c1abf838cf4e7"
 
   depends_on 'mulle-bootstrap'
   depends_on 'cmake'
