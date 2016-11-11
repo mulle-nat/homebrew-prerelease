a9117eed04276fac79d7cac06c0a53d8f2f94252
Nat! <nat@mulle-kybernetik.com>
2016-11-11 22:45:13 +0100

    fix

--- nil-clang.rb
+++ nil-clang.rb
@@ -1,15 +1,21 @@
 class NilClang < Formula
    homepage "https://github.com/mulle-nat/nil-clang"
    desc "Nil Clang is a shell script that calls clang"
-   url "https://github.com/mulle-nat/nil-clang/archive/1.0.tar.gz"
-   sha256 "5a01af88d84adddd5be3afd645f8a13a613a00df34e8c8ff7bf04c48566c7f79"
+   url "https://github.com/mulle-nat/nil-clang/archive/1.1.tar.gz"
+   sha256 "bb3733b401677eafe70a96f46707e440ea5bb9d8e54666477ac43d1cb2b5dd08"
 
    #
    # homebrew llvm is built with polly, but cmake doesn't pick it up
    # for some reason
    #
    def install
-      bin.install_symlink prefix/"nil-clang"
+      mkdir "build" do
+         system "cmake", "-G", "Unix Makefiles", *std_cmake_args
+         system "make", ENV[ "MAKEFLAGS"]
+         system "make install"
+
+         bin.install_symlink prefix/"bin/nil-clang" => "nil-clang"
+      end
 
       #
       # install a shim for nil-lang into homebrew
