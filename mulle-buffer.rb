ca4d87b11ea101e6f522b6079b7d3042559eed0d
Nat! <nat@mulle-kybernetik.com>
2016-12-05 18:55:36 +0100

    0.3.1 release of mulle-buffer

--- mulle-buffer.rb
+++ mulle-buffer.rb
@@ -1,9 +1,9 @@
 class MulleBuffer < Formula
    homepage "https://github.com/mulle-nat/mulle-buffer"
    desc "A growable C array of unsigned chars"
-   url "https://github.com/mulle-nat/mulle-buffer/archive/0.2.7.tar.gz"
-   version "0.2.7"
-   sha256 "069347a82b5d201584470935c6fd64663f3bd921383fe00c7ee97589aaf603f6"
+   url "https://github.com/mulle-nat/mulle-buffer/archive/0.3.1.tar.gz"
+   version "0.3.1"
+   sha256 "38f9aa93576b9cf46ffdf96234e60f9e4b0a6f961d3ed2f92c501c058c56c2f0"
 
    depends_on 'mulle-kybernetik/software/mulle-c11'
    depends_on 'mulle-kybernetik/software/mulle-allocator'
