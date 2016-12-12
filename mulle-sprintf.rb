00a873a0f45db07fcfcb1d7d6617dfd9ada7a64e
Nat! <nat@mulle-kybernetik.com>
2016-12-12 22:14:11 +0100

    0.7.7 release of mulle-sprintf

--- mulle-sprintf.rb
+++ mulle-sprintf.rb
@@ -1,9 +1,9 @@
 class MulleSprintf < Formula
    homepage "https://github.com/mulle-nat/mulle-sprintf"
    desc " An extensible sprintf function supporting stdarg and mulle-vararg"
-   url "https://github.com/mulle-nat/mulle-sprintf/archive/0.7.5.tar.gz"
-   version "0.7.5"
-   sha256 "37550df51ad188a57a535d0e911796618e0cd40a00edecd47621346b0adfb1e2"
+   url "https://github.com/mulle-nat/mulle-sprintf/archive/0.7.7.tar.gz"
+   version "0.7.7"
+   sha256 "1f913ab5cd3600319e1c030e945ca68e9cec949d48157100d0732e5729efdc71"
 
    depends_on 'mulle-kybernetik/software/mulle-buffer'
    depends_on 'mulle-kybernetik/software/mulle-utf'
@@ -11,7 +11,7 @@ class MulleSprintf < Formula
    depends_on 'mulle-kybernetik/software/mulle-build' => :build
 
    def install
-      system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
+      system "mulle-install", "-e", "--prefix", "#{prefix}"
    end
 
    test do
