804c0f12c2096042cf54bf69ced3b845cc8a03ae
Nat! <nat@mulle-kybernetik.com>
2016-12-13 11:15:56 +0100

    0.7.11 release of mulle-sprintf

--- mulle-sprintf.rb
+++ mulle-sprintf.rb
@@ -1,9 +1,9 @@
 class MulleSprintf < Formula
    homepage "https://github.com/mulle-nat/mulle-sprintf"
    desc " An extensible sprintf function supporting stdarg and mulle-vararg"
-   url "https://github.com/mulle-nat/mulle-sprintf/archive/0.7.9.tar.gz"
-   version "0.7.9"
-   sha256 "3971725f3204593ccdf404e260f22afb22dafbb8c525a103abd044003704178d"
+   url "https://github.com/mulle-nat/mulle-sprintf/archive/0.7.11.tar.gz"
+   version "0.7.11"
+   sha256 "b204f0d0db153697bcce1c60ec7c3464a5fd7042b772593d7c909c3f1f91a756"
 
    depends_on 'mulle-kybernetik/software/mulle-buffer'
    depends_on 'mulle-kybernetik/software/mulle-utf'
