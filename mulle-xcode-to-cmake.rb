b50b28b9f3d9b0c4a9c1a63271cd14979364903a
Nat! <nat@mulle-kybernetik.com>
2017-02-21 18:46:37 +0100

    0.4.2 release of mulle-xcode-to-cmake

--- mulle-xcode-to-cmake.rb
+++ mulle-xcode-to-cmake.rb
@@ -1,9 +1,9 @@
 class MulleXcodeToCmake < Formula
   homepage "http://www.mulle-kybernetik.com/software/git/mulle-xcode-to-cmake"
   desc "Convert Xcode projects to cmake CMakeLists.txt "
-  url "http://www.mulle-kybernetik.com/software/git/mulle-xcode-to-cmake/tarball/0.4.1"
-  version "0.4.1"
-  sha256 "287881b35a4b6c3534c1a27fe2919529ff94ec62520a1893cf6e0f38644cc0f4"
+  url "http://www.mulle-kybernetik.com/software/git/mulle-xcode-to-cmake/tarball/0.4.2"
+  version "0.4.2"
+  sha256 "30bbae70f9caf0da896e249ad0d3a848ee49c82ac9a46c1dbe7562fe734ca461"
 
   depends_on :xcode => :build
   depends_on :macos => :snow_leopard
