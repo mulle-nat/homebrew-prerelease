9fbbb5af05faeed63d965ee8e3bce1f5027d6f84
Nat! <nat@codeon.de>
2017-03-31 13:52:58 +0200

    3.3.1 release of mulle-thread

--- mulle-thread.rb
+++ mulle-thread.rb
@@ -1,12 +1,15 @@
 class MulleThread < Formula
    homepage "https://github.com/mulle-nat/mulle-thread"
    desc "Cross-platform threads and atomic operations"
-   url "https://github.com/mulle-nat/mulle-thread/archive/3.2.15.tar.gz"
-   version "3.2.15"
-   sha256 "ebad984d660e6ffc41c7cd689ac71b181cc2fa3f6cda7c36e18fff93d56e83b0"
+   url "https://github.com/mulle-nat/mulle-thread/archive/3.3.1.tar.gz"
+   version "3.3.1"
+   sha256 "ee572e3cca4b803076cd5acfe210d44169ec580e1960a4a8c5ddb2aaca879c2a"
 
    depends_on 'mulle-kybernetik/software/mulle-c11'
+
    depends_on 'mulle-kybernetik/software/mulle-build' => :build
+   depends_on 'mulle-kybernetik/software/mulle-bootstrap' => :build
+   depends_on 'cmake' => :build
 
    def install
       system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
