f9b6788ffc7103a0188f84665c0128d0ce0e1980
Nat! <nat@mulle-kybernetik.com>
2017-04-29 01:28:53 +0200

    0.8.9 release of mulle-container

--- mulle-container.rb
+++ mulle-container.rb
@@ -1,13 +1,16 @@
 class MulleContainer < Formula
    homepage "https://github.com/mulle-nat/mulle-container"
    desc "Sets, hashtables, byte buffers and more, written in C"
-   url "https://github.com/mulle-nat/mulle-container/archive/0.8.7.tar.gz"
-   version "0.8.7"
-   sha256 "43a4e2b474dcccabf564466afe18c20db8b8391c987f75495d158800c3ea1473"
+   url "https://github.com/mulle-nat/mulle-container/archive/0.8.9.tar.gz"
+   version "0.8.9"
+   sha256 "f2521871bbd8521ab2cf8792dbf6f4571224d34975e616ca22f8615e7101b96a"
 
    depends_on 'mulle-kybernetik/software/mulle-c11'
    depends_on 'mulle-kybernetik/software/mulle-allocator'
+
    depends_on 'mulle-kybernetik/software/mulle-build' => :build
+   depends_on 'mulle-kybernetik/software/mulle-bootstrap' => :build
+   depends_on 'cmake' => :build
 
    def install
       system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
