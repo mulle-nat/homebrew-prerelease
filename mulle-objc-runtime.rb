b2bcaf79035f5a5aae3d7fee4941697935750b2f
Nat! <nat@mulle-kybernetik.com>
2017-05-01 18:07:05 +0200

    0.5.11 release of mulle-objc-runtime

--- mulle-objc-runtime.rb
+++ mulle-objc-runtime.rb
@@ -3,9 +3,9 @@ class MulleObjcRuntime < Formula
    desc "An Objective-C runtime, written 100% in C"
    url "https://github.com/mulle-nat/mulle-objc-runtime/archive/0.5.11.tar.gz"
    version "0.5.11"
-   sha256 "fd0a9e2a87149338e4ebd4785358a46fcc03f542b700b0ea16857c2d0898a873"
-   depends_on 'mulle-nat/software/mulle-concurrent'
-   depends_on 'mulle-nat/software/mulle-vararg'
+   sha256 "211ddf8c97650174e6546e6bd252d935381462b22317c2e0fff188044036970c"
+   depends_on 'mulle-kybernetik/software/mulle-concurrent'
+   depends_on 'mulle-kybernetik/software/mulle-vararg'
    depends_on 'software/alpha/mulle-build' => :build
 
    def install
