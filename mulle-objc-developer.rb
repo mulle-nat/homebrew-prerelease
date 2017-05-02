2787e36411433c72eda31d1346670efcb7d328e8
Nat! <nat@codeon.de>
2017-05-02 13:33:09 +0200

    0.5.1.1 release of mulle-objc-developer

--- mulle-objc-developer.rb
+++ mulle-objc-developer.rb
@@ -1,22 +1,16 @@
 class MulleObjcDeveloper < Formula
    homepage "https://github.com/mulle-nat/mulle-objc-developer"
    desc "mulle-objc Developer Environment"
-   url "https://github.com/mulle-nat/mulle-objc-developer/archive/0.5.1.tar.gz"
-   version "0.5.1"
-   sha256 "c6d8f61adcc1e198cbe08baca1f559bae18f2760d9acbb774b9a193b3d188f37"
-   depends_on 'mulle-kybernetik/software/mulle-allocator'
-   depends_on 'mulle-kybernetik/software/mulle-c11'
-   depends_on 'mulle-kybernetik/software/mulle-concurrent'
-   depends_on 'mulle-kybernetik/software/mulle-container'
-   depends_on 'mulle-kybernetik/software/mulle-vararg'
-   depends_on 'mulle-kybernetik/software/mulle-thread'
-   depends_on 'mulle-kybernetik/software/mulle-objc-runtime'
-   depends_on 'mulle-kybernetik/software/MulleObjC'
+   url "https://github.com/mulle-nat/mulle-objc-developer/archive/0.5.1.1.tar.gz"
+   version "0.5.1.1"
+   sha256 "fa59f6a781b8f55695bdde55bbdb7750e69d7e9af23a7e1e07f4a291049e945b"
+   depends_on 'mulle-kybernetik/alpha/mulle-boostrap'
+   depends_on 'mulle-kybernetik/alpha/mulle-build'
    depends_on 'codeon-gmbh/software/mulle-clang'
    depends_on 'mulle-kybernetik/alpha/mulle-build' => :build
 
    def install
-      system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
+      system "mulle-install", "-e", "--prefix", "#{prefix}", "--homebrew"
    end
 
    test do
