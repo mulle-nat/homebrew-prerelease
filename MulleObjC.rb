4cfca1ac497801277d91fef28fdd1e7d8f9e0890
Nat! <nat@mulle-kybernetik.com>
2017-05-31 16:51:48 +0200

    0.8.1 release of MulleObjC

--- MulleObjC.rb
+++ MulleObjC.rb
@@ -1,23 +1,25 @@
 class Mulleobjc < Formula
-   homepage "https://github.com/mulle-nat/MulleObjC"
-   desc "Objective-C root classes using the mulle-objc runtime"
-   url "https://github.com/mulle-nat/MulleObjC/archive/0.1.7.tar.gz"
-   version "0.1.7"
-   sha256 "13a5a7173cb1a8215c9c1559649c3142161d230c0a80b923427c10238f4ff3c1"
+  desc "Objective-C root classes using the mulle-objc runtime"
+  homepage "https://github.com/mulle-nat/MulleObjC"
+  url "https://github.com/mulle-nat/MulleObjC/archive/0.8.1.tar.gz"
+  sha256 "98a67e3317dc3804b2552bab310a540ded0db6451af57f888bf299db3e4510ff"
+  # version "0.8.1"
 
-   depends_on 'mulle-kybernetik/software/mulle-objc-runtime'
-   depends_on 'mulle-kybernetik/software/mulle-container'
+  depends_on "mulle-kybernetik/software//mulle-objc-runtime"
+  depends_on "mulle-kybernetik/software//mulle-container"
 
-   depends_on 'mulle-kybernetik/software/mulle-build' => :build
-   depends_on 'mulle-kybernetik/software/mulle-bootstrap' => :build
-   depends_on 'cmake' => :build
+  depends_on "mulle-kybernetik/software/mulle-bootstrap" => :build
+  depends_on "mulle-kybernetik/software/mulle-build" => :build
+  depends_on "cmake" => :build
 
-   def install
-      system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
-   end
+  def install
+    system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
+  end
 
-   test do
-      system "mulle-test"
-   end
+  test do
+    if File.directory? 'tests'
+      system "mulle-test", "-vvv", "--fast-test"
+    end
+  end
 end
 # FORMULA MulleObjC.rb
