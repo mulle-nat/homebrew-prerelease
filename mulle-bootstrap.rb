61cd7029e721c6aedcbef3c1a22b5d57492ae8eb
Nat! <nat@codeon.de>
2017-06-02 18:16:42 +0200

    3.6.3 release of mulle-bootstrap

--- mulle-bootstrap.rb
+++ mulle-bootstrap.rb
@@ -1,9 +1,9 @@
 class MulleBootstrap < Formula
   homepage "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap"
   desc "Dependency manager for cross-platform projects"
-  url "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap/tarball/3.6.2"
-  version "3.6.2"
-  sha256 "887fed2950df9bfaee88ee86bbd6910eaa128df74bd6faea68fcc62257997d41"
+  url "http://www.mulle-kybernetik.com/software/git/mulle-bootstrap/tarball/3.6.3"
+  version "3.6.3"
+  sha256 "c3a27c65ab71c8d1addc56ee690197dd505b00c5389e114a3ef5d45e8318e352"
 
   def install
      system "./install.sh", "#{prefix}"
