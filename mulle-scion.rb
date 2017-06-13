0a9186276be985b0e71e0c6bc74af6334395149f
Nat! <nat@codeon.de>
2017-06-13 18:20:36 +0200

    1856 release of mulle-scion

--- mulle-scion.rb
+++ mulle-scion.rb
@@ -2,7 +2,7 @@ class MulleScion < Formula
   desc "A modern template engine for Objective C"
   homepage "https://github.com/mulle-nat/MulleScion"
   url "https://github.com/mulle-nat/MulleScion/archive/1856.tar.gz"
-  sha256 "2fe1083364ccbeaee71868221243b1ffad80507beac4496e32183781381bec99"
+  sha256 "4d4e8815d69be3950362f2ba18b80a0dfd9ce9d70a1a82b16aa4f901e968be3c"
   # version "1856"
 
   depends_on "mulle-kybernetik/software/mulle-build" => :build
