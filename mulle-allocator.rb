7f222dab59c4a9892a58820b898d5fc7e0fd1598
Nat! <nat@mulle-kybernetik.com>
2017-06-17 18:56:23 +0200

    2.1.9 release of mulle-allocator

--- mulle-allocator.rb
+++ mulle-allocator.rb
@@ -1,23 +1,21 @@
 class MulleAllocator < Formula
-   homepage "https://github.com/mulle-nat/mulle-allocator"
-   desc "Flexible C memory allocation scheme with leak checking"
-   url "https://github.com/mulle-nat/mulle-allocator/archive/2.1.7.tar.gz"
-   version "2.1.7"
-   sha256 "69819bf18093c958dda9725206029a7b7a393e8d90fced5eed13d99141f035ba"
+  desc "Flexible C memory allocation scheme with leak checking"
+  homepage "https://github.com/mulle-nat/mulle-allocator"
+  url "https://github.com/mulle-nat/mulle-allocator/archive/2.1.9.tar.gz"
+  sha256 "1ca459ecc5657511c70dc58017fa65b64fb773482453a9a8da151c413e5d0b87"
+  # version "2.1.9"
 
-   depends_on 'mulle-kybernetik/software/mulle-c11'
-   depends_on 'mulle-kybernetik/software/mulle-thread'
+  depends_on "mulle-kybernetik/software/mulle-c11"
+  depends_on "mulle-kybernetik/software/mulle-thread"
 
-   depends_on 'mulle-kybernetik/software/mulle-build' => :build
-   depends_on 'mulle-kybernetik/software/mulle-bootstrap' => :build
-   depends_on 'cmake' => :build
+  def install
+    system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
+  end
 
-   def install
-      system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
-   end
-
-   test do
-      system "mulle-test"
-   end
+  test do
+    if File.directory? 'tests'
+      system "mulle-test", "-vvv", "--fast-test"
+    end
+  end
 end
 # FORMULA mulle-allocator.rb
