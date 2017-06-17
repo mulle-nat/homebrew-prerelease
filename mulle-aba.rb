ceaff8fb24658332724f31c558c79b9dafc3db94
Nat! <nat@mulle-kybernetik.com>
2017-06-17 19:39:12 +0200

    1.4.7 release of mulle-aba

--- mulle-aba.rb
+++ mulle-aba.rb
@@ -1,21 +1,22 @@
 class MulleAba < Formula
-   homepage "https://github.com/mulle-nat/mulle-aba"
-   desc "A portable, lock-free solution to the ABA problem in C"
-   url "https://github.com/mulle-nat/mulle-aba/archive/1.4.5.tar.gz"
-   version "1.4.5"
-   sha256 "d32570b79ea42e00a1a2f6fd1512c0c5b73f5f450d4715edbbfe6ed6202be451"
+  desc "A portable, lock-free solution to the ABA problem in C"
+  homepage "https://github.com/mulle-nat/mulle-aba"
+  url "https://github.com/mulle-nat/mulle-aba/archive/1.4.7.tar.gz"
+  sha256 "402255354ab281bffc44297c358d8dd9a4135747de722bdd4ac8e67b55424719"
+  # version "1.4.7"
 
-   depends_on 'mulle-kybernetik/software/mulle-c11'
-   depends_on 'mulle-kybernetik/software/mulle-allocator'
-   depends_on 'mulle-kybernetik/software/mulle-thread'
-   depends_on 'mulle-kybernetik/software/mulle-build' => :build
+  depends_on "mulle-kybernetik/software/mulle-c11"
+  depends_on "mulle-kybernetik/software/mulle-allocator"
+  depends_on "mulle-kybernetik/software/mulle-thread"
 
-   def install
-      system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
-   end
+  def install
+    system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
+  end
 
-   test do
-      system "mulle-test"
-   end
+  test do
+    if File.directory? 'tests'
+      system "mulle-test", "-vvv", "--fast-test"
+    end
+  end
 end
 # FORMULA mulle-aba.rb
