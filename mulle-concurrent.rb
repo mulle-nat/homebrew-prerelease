1933e6b2e1bb3c523569b55cc683be68113ebae3
Nat! <nat@mulle-kybernetik.com>
2017-06-17 19:06:34 +0200

    1.3.3 release of mulle-concurrent

--- mulle-concurrent.rb
+++ mulle-concurrent.rb
@@ -1,24 +1,22 @@
 class MulleConcurrent < Formula
-   homepage "https://github.com/mulle-nat/mulle-concurrent"
-   desc "Lock- and Wait-free Hashtable (and an Array too) in C"
-   url "https://github.com/mulle-nat/mulle-concurrent/archive/1.3.1.tar.gz"
-   version "1.3.1"
-   sha256 "85ce8ec99ed258a6346c2e8402e7e614a1f4621634e849f1c5e7a14728d0597b"
+  desc "Lock- and Wait-free Hashtable (and an Array too) in C"
+  homepage "https://github.com/mulle-nat/mulle-concurrent"
+  url "https://github.com/mulle-nat/mulle-concurrent/archive/1.3.3.tar.gz"
+  sha256 "7d545c4ef88a5350ed98e934b8af96b34d1ee27e71a43bcdfbc6685140579906"
+  # version "1.3.3"
 
-   depends_on 'mulle-kybernetik/software/mulle-allocator'
-   depends_on 'mulle-kybernetik/software/mulle-aba'
-   depends_on 'mulle-kybernetik/software/mulle-thread'
+  depends_on "mulle-kybernetik/software/mulle-allocator"
+  depends_on "mulle-kybernetik/software/mulle-aba"
+  depends_on "mulle-kybernetik/software/mulle-thread"
 
-   depends_on 'mulle-kybernetik/software/mulle-build' => :build
-   depends_on 'mulle-kybernetik/software/mulle-bootstrap' => :build
-   depends_on 'cmake' => :build
+  def install
+    system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
+  end
 
-   def install
-      system "mulle-install", "--prefix", "#{prefix}", "--homebrew"
-   end
-
-   test do
-      system "mulle-test"
-   end
+  test do
+    if File.directory? 'tests'
+      system "mulle-test", "-vvv", "--fast-test"
+    end
+  end
 end
 # FORMULA mulle-concurrent.rb
