755bf3939144a940ec18b70198bd06c014d13b52
Nat! <nat@mulle-kybernetik.com>
2017-06-18 20:26:05 +0200

    0.8.5 release of MulleObjC

--- MulleObjC.rb
+++ MulleObjC.rb
@@ -1,17 +1,12 @@
 class Mulleobjc < Formula
-  desc "Objective-C root classes using the mulle-objc runtime"
+  desc "MyProject does this and that"
   homepage "https://github.com/mulle-nat/MulleObjC"
-  url "https://github.com/mulle-nat/MulleObjC/archive/0.8.3.tar.gz"
-  sha256 "e8064f363f6b1bb65dc2da9e58f7d6f9e7b23983a792180b17a82db14d1ab40a"
-  # version "0.8.3"
+  url "https://github.com/mulle-nat/MulleObjC/archive/0.8.5.tar.gz"
+  sha256 "628e20e530fe8411f96f1bf65af1e7f8c5bac7b5fe664de7b14b88f8a34fd24c"
+  # version "0.8.5"
 
   depends_on "mulle-kybernetik/software/mulle-objc-runtime"
   depends_on "mulle-kybernetik/software/mulle-objc-list"
-  depends_on "mulle-kybernetik/software/mulle-container"
-
-  depends_on "mulle-kybernetik/software/mulle-bootstrap" => :build
-  depends_on "mulle-kybernetik/software/mulle-build" => :build
-  depends_on "cmake" => :build
 
   def install
     system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
