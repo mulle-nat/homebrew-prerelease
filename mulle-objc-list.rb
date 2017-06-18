1a9fcb54c24e6513b2b214d9b4f85b0aace7d5db
Nat! <nat@mulle-kybernetik.com>
2017-06-18 20:42:14 +0200

    0.8.3 release of mulle-objc-list

--- mulle-objc-list.rb
+++ mulle-objc-list.rb
@@ -1,13 +1,11 @@
 class MulleObjcList < Formula
   desc "mulle-objc-list list classes and categories or methods of a library"
   homepage "https://github.com/mulle-nat/mulle-objc-list"
-  url "https://github.com/mulle-nat/mulle-objc-list/archive/0.8.1.tar.gz"
-  sha256 "6eec428f2c3db4d4d134ce0084b946e0e541e95ea2a1f11c6b2e2cdb743e7e12"
-  # version "0.8.1"
+  url "https://github.com/mulle-nat/mulle-objc-list/archive/0.8.3.tar.gz"
+  sha256 "2d3b4ced81187ded72c9d606a22cc80effba92847ef1b8e03e99aca805c592b3"
+  # version "0.8.3"
 
-  depends_on "mulle-kybernetik/software/mulle-bootstrap" => :build
-  depends_on "mulle-kybernetik/software/mulle-build" => :build
-  depends_on "cmake" => :build
+  depends_on "mulle-kybernetik/software/mulle-objc-runtime"
 
   def install
     system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
