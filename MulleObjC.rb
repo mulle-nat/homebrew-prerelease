33ba0b0cac1b696280deaea17e37ad70a6753b5d
Nat! <nat@mulle-kybernetik.com>
2017-06-18 22:40:36 +0200

    0.8.5 release of MulleObjC

--- MulleObjC.rb
+++ MulleObjC.rb
@@ -2,7 +2,7 @@ class Mulleobjc < Formula
   desc "MyProject does this and that"
   homepage "https://github.com/mulle-nat/MulleObjC"
   url "https://github.com/mulle-nat/MulleObjC/archive/0.8.5.tar.gz"
-  sha256 "628e20e530fe8411f96f1bf65af1e7f8c5bac7b5fe664de7b14b88f8a34fd24c"
+  sha256 "c751493bccc3989a716d74f8fab2ff2a144e7ca6e7853727aa4366c054e334b5"
   # version "0.8.5"
 
   depends_on "mulle-kybernetik/software/mulle-objc-runtime"
