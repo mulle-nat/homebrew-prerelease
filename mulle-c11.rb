bdbb184261dcdde14dfdfddcfb2e67313f12e7f3
Nat! <nat@mulle-kybernetik.com>
2017-06-18 21:22:23 +0200

    1.2.1 release of mulle-c11

--- mulle-c11.rb
+++ mulle-c11.rb
@@ -2,7 +2,7 @@ class MulleC11 < Formula
   desc "Cross-platform compiler glue"
   homepage "https://github.com/mulle-nat/mulle-c11"
   url "https://github.com/mulle-nat/mulle-c11/archive/1.2.1.tar.gz"
-  sha256 "9ede9a6d235b2143f4fc4e2d173554ee7a9b033606436ac03cce605cd1ecf99c"
+  sha256 "dfe2d2846b64271ecb7264c16d912568c77bb8713f48e8843cabb95562ef1e1b"
   # version "1.2.1"
 
   def install
