9e07f24688f71f4c330fc87adda5840ecd447f6c
Nat! <nat@codeon.de>
2017-06-21 16:54:17 +0200

    9.0.1 release of mulle-objc-tools

--- mulle-objc-tools.rb
+++ mulle-objc-tools.rb
@@ -1,9 +1,9 @@
 class MulleObjcTools < Formula
   desc "🔧Tools for mulle-objc development and to maintain mulle-objc forks"
   homepage "https://github.com/mulle-nat/mulle-objc-tools"
-  url "https://github.com/mulle-nat/mulle-objc-tools/archive/0.9.1.tar.gz"
-  sha256 "e464cdc2adee05a53628723f9267ece88aa555c366f22765450a770dd75780d8"
-  # version "0.9.1"
+  url "https://github.com/mulle-nat/mulle-objc-tools/archive/9.0.1.tar.gz"
+  sha256 "755eab9b4b3cbf917ba162ed542ed7c5fb60c0319146bd3aaa2112bf7850aaa4"
+  # version "9.0.1"
 
   depends_on "mulle-kybernetik/software/mulle-build" => :build
   depends_on "mulle-kybernetik/software/mulle-bootstrap" => :build
