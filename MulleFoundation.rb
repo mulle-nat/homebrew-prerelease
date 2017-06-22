9fa3a712650789906143bdce63ab6fa2c5faa2a5
Nat! <nat@mulle-kybernetik.com>
2017-06-22 19:32:32 +0200

    0.9.1 release of MulleFoundation

--- MulleFoundation.rb
+++ MulleFoundation.rb
@@ -2,7 +2,7 @@ class Mullefoundation < Formula
   desc "💎 The Foundation library for mulle-objc"
   homepage "https://github.com/mulle-nat/MulleFoundation"
   url "https://github.com/mulle-nat/MulleFoundation/archive/0.9.1.tar.gz"
-  sha256 "65c96ec49892cf9b7f26558a313af16c693817c041c6a27e60657978c9d6c167"
+  sha256 "8470e98f7b09fe1bec13d1ed4701b6bdd49365b304e0d25a469a11351ad1145b"
   # version "0.9.1"
 
   depends_on "mulle-kybernetik/software/mulle-build" => :build
