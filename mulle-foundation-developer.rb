7e7ed0eb7da153afe675556a809d057f3b1af18d
Nat! <nat@mulle-kybernetik.com>
2017-06-25 18:27:36 +0200

    0.9.1 release of mulle-foundation-developer

--- mulle-foundation-developer.rb
+++ mulle-foundation-developer.rb
@@ -2,7 +2,7 @@ class MulleFoundationDeveloper < Formula
   desc "🎁 Developer tools for mulle-objc with MulleFoundation support"
   homepage "https://github.com/mulle-nat/mulle-foundation-developer"
   url "https://github.com/mulle-nat/mulle-foundation-developer/archive/0.9.1.tar.gz"
-  sha256 "690f125063c649821467f7959feb2ceb4470657a1f4ce7337736ba623d6f1978"
+  sha256 "a3ac87450e6223a86ca99e2229286eb3f4d41a4a50b7cbb88ee57aefb205b4ad"
   # version "0.9.1"
 
   depends_on "mulle-kybernetik/software/mulle-objc-developer"
