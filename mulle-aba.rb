10a08898e8e8df874e1dc8db8a93cb952ee1c318
Nat! <nat@codeon.de>
2017-07-03 11:36:30 +0200

    1.4.9 release of mulle-aba

--- mulle-aba.rb
+++ mulle-aba.rb
@@ -1,9 +1,9 @@
 class MulleAba < Formula
   desc "A portable, lock-free solution to the ABA problem in C"
   homepage "https://github.com/mulle-nat/mulle-aba"
-  url "https://github.com/mulle-nat/mulle-aba/archive/1.4.7.tar.gz"
-  sha256 "402255354ab281bffc44297c358d8dd9a4135747de722bdd4ac8e67b55424719"
-  # version "1.4.7"
+  url "https://github.com/mulle-nat/mulle-aba/archive/1.4.9.tar.gz"
+  sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
+  # version "1.4.9"
 
   depends_on "mulle-kybernetik/software/mulle-c11"
   depends_on "mulle-kybernetik/software/mulle-allocator"
