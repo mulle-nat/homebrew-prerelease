8e4eccab96305321aa5cfa5e765e3e0849230de2
Nat! <nat@codeon.de>
2017-07-03 11:47:43 +0200

    1.0.13 release of mulle-utf

--- mulle-utf.rb
+++ mulle-utf.rb
@@ -1,9 +1,9 @@
 class MulleUtf < Formula
   desc "UTF8-16-32 analysis, conversion, classification library"
   homepage "https://github.com/mulle-nat/mulle-utf"
-  url "https://github.com/mulle-nat/mulle-utf/archive/1.0.11.tar.gz"
-  sha256 "42f7faf0c51b2e013e69e520b5c167b6354ca5fb74be4d404f75617b3b6b815b"
-  # version "1.0.11"
+  url "https://github.com/mulle-nat/mulle-utf/archive/1.0.13.tar.gz"
+  sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
+  # version "1.0.13"
 
   depends_on "mulle-kybernetik/software/mulle-allocator"
 
