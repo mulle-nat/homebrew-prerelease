66ba4e46d37a3d95c8e7f5141b0f1d5006e4dee6
Nat! <nat@mulle-kybernetik.com>
2017-07-05 16:45:22 +0200

    2.1.11 release of mulle-allocator

--- mulle-allocator.rb
+++ mulle-allocator.rb
@@ -2,7 +2,7 @@ class MulleAllocator < Formula
   desc "Flexible C memory allocation scheme with leak checking"
   homepage "https://github.com/mulle-nat/mulle-allocator"
   url "https://github.com/mulle-nat/mulle-allocator/archive/2.1.11.tar.gz"
-  sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
+  sha256 "7d34dfa6798c8a949adae914694d8f878599b962467172b1455ade9cd682d437"
   # version "2.1.11"
 
   depends_on "mulle-kybernetik/software/mulle-c11"
