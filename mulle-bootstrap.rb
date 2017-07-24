5a879d402ef3e3401174021141ac404fe27c07f8
Nat! <nat@codeon.de>
2017-07-24 16:52:53 +0200

    3.9.0 release of mulle-bootstrap

--- mulle-bootstrap.rb
+++ mulle-bootstrap.rb
@@ -2,7 +2,7 @@ class MulleBootstrap < Formula
   desc "👢 Cross platform dependency manager for developers"
   homepage "https://github.com/mulle-nat/mulle-bootstrap"
   url "https://github.com/mulle-nat/mulle-bootstrap/archive/3.9.0.tar.gz"
-  sha256 "76f87e230488eb73fddc43cb2489a9724769d1f3128677c0d699aec0a0f8faf3"
+  sha256 "26db0e58333ff5af65e9ce193f6d1140a6b7e6cf304f464c43a330a1638d071d"
   # version "3.9.0"
   def install
      system "./install.sh", "#{prefix}"
