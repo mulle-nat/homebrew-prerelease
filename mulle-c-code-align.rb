d3fb3086271ff0e640e8fc55e411e583c806271f
Nat! <nat@mulle-kybernetik.com>
2017-08-10 15:29:13 +0200

    1.1.4 release of mulle-c-code-align

--- mulle-c-code-align.rb
+++ mulle-c-code-align.rb
@@ -1,11 +1,11 @@
 class MulleCCodeAlign < Formula
   desc "☛ Aligns assignments and variable declarations."
   homepage "https://github.com/mulle-nat/mulle-c-code-align"
-  url "https://github.com/mulle-nat/mulle-c-code-align/archive/1.1.3.tar.gz"
-  sha256 "0697c2d691a4e80131ba9a27c5d7d0aefa92b30e73b78dbd77f7ec94a22bab3c"
-  # version "1.1.3"
+  url "https://github.com/mulle-nat/mulle-c-code-align/archive/1.1.4.tar.gz"
+  sha256 "3b3a827372724f814f2906fb2817e832c4959a1791028f45c3f51f7d57a9390c"
+  # version "1.1.4"
 def install
-   system "make", "install", "INSTALL_PATH=#{prefix}"
+   system "make", "install", "INSTALL_PREFIX=#{prefix}"
 end
 end
 # FORMULA mulle-c-code-align.rb
