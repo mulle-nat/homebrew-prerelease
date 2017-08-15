c559bbffd9705a7e14c17f36c938de3b3865021a
Nat! <nat@codeon.de>
2017-08-15 16:49:56 +0200

    1.2.4 release of mulle-xcode-settings

--- mulle-xcode-settings.rb
+++ mulle-xcode-settings.rb
@@ -1,15 +1,15 @@
 class MulleXcodeSettings < Formula
   desc "🗿 Edit Xcode build settings from the command line"
   homepage "https://github.com/mulle-nat/mulle-xcode-settings"
-  url "https://github.com/mulle-nat/mulle-xcode-settings/archive/1.2.3.tar.gz"
-  sha256 "74e406e523cab3c2b05e9cb197265c9d06b8143737a4b94d59a29d592187d1ae"
-  # version "1.2.3"
+  url "https://github.com/mulle-nat/mulle-xcode-settings/archive/1.2.4.tar.gz"
+  sha256 "783ac3b7ffb191dd95286613714e26a00ce085609d33c1576bdeb4743dffb4b4"
+  # version "1.2.4"
 
   depends_on "mulle-kybernetik/software/mulle-build" => :build
   depends_on "mulle-kybernetik/software/mulle-bootstrap" => :build
 
   def install
-    system "xcodebuild", "-configuration", "Release", "DSTROOT=#prefix", "install"
+    system "xcodebuild", "-configuration", "Release", "DSTROOT=#{prefix}", "install"
   end
 
   test do
