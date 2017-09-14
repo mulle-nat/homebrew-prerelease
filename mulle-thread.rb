1b5f295311bf708f13678d059f4cb37d65c87a96
Nat! <nat@mulle-kybernetik.com>
2017-09-14 20:12:29 +0200

    3.3.11 release of mulle-thread

--- mulle-thread.rb
+++ mulle-thread.rb
@@ -1,9 +1,9 @@
 class MulleThread < Formula
   desc "Cross-platform threads and atomic operations"
   homepage "https://github.com/mulle-nat/mulle-thread"
-  url "https://github.com/mulle-nat/mulle-thread/archive/3.3.9.tar.gz"
-  sha256 "8fdd2ca848464db864a411a84f57839873ff762595c0800f81a22fe9aa31c7b8"
-  # version "3.3.9"
+  url "https://github.com/mulle-nat/mulle-thread/archive/3.3.11.tar.gz"
+  sha256 "be6279922f73632d43a03a6eddffaa35eac52a0301f51946f9cc27bcf37b16cc"
+  # version "3.3.11"
 
   depends_on "mulle-kybernetik/software/mulle-c11"
 
