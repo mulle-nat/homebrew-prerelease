47d2793d5920ce817b544149b672936616ed4f46
Nat! <nat@mulle-kybernetik.com>
2017-09-18 20:12:59 +0200

    1.4.7 release of mulle-concurrent

--- mulle-concurrent.rb
+++ mulle-concurrent.rb
@@ -1,9 +1,9 @@
 class MulleConcurrent < Formula
   desc "Lock- and Wait-free Hashtable (and an Array too) in C"
   homepage "https://github.com/mulle-nat/mulle-concurrent"
-  url "https://github.com/mulle-nat/mulle-concurrent/archive/1.4.5.tar.gz"
-  sha256 "d92c031bafc80f17e19601adee50b8830a88a7b6c34001e41194ebdf7701b102"
-  # version "1.4.5"
+  url "https://github.com/mulle-nat/mulle-concurrent/archive/1.4.7.tar.gz"
+  sha256 "7a56a230e1be690f85fce9ba27436daaef6d5ef00b61110c984269e8814dc244"
+  # version "1.4.7"
 
   depends_on "mulle-kybernetik/software/mulle-allocator"
   depends_on "mulle-kybernetik/software/mulle-aba"
