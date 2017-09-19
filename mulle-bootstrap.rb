2a4bb64b9141a54a34cd71b9d5bf23801bc694e6
Nat! <nat@codeon.de>
2017-09-19 14:25:21 +0200

    3.12.2 release of mulle-bootstrap

--- mulle-bootstrap.rb
+++ mulle-bootstrap.rb
@@ -1,14 +1,11 @@
 class MulleBootstrap < Formula
   desc "👢 Cross platform dependency manager for developers"
   homepage "https://github.com/mulle-nat/mulle-bootstrap"
-  url "https://github.com/mulle-nat/mulle-bootstrap/archive/3.12.1.tar.gz"
-  sha256 "44cb05d9b8fe16a0a5571151bf17d0c4e36e42c23b68b90d4b28607adce2b6c6"
-  # version "3.12.1"
-  def install
-     system "./install.sh", "#{prefix}"
-  end
-
-  test do
-  end
+  url "https://github.com/mulle-nat/mulle-bootstrap/archive/3.12.2.tar.gz"
+  sha256 "b218122b4f161fe943f5894cfa9e88aae68eaa4b3d514437eea908e36b09c52a"
+  # version "3.12.2"
+def install
+  system "./install.sh", "#{prefix}"
+end
 end
 # FORMULA mulle-bootstrap.rb
