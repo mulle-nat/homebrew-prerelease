90075f5a5af9706ef1ce3cbbccd334f7481b66f6
Nat! <nat@codeon.de>
2017-09-19 16:24:27 +0200

    0.1.1 release of mulle-sde

--- mulle-sde.rb
+++ mulle-sde.rb
@@ -1,9 +1,9 @@
 class MulleSde < Formula
   desc "🌳 Virtual environment for C and Objective-C"
   homepage "https://github.com/mulle-nat/mulle-sde"
-  url "https://github.com/mulle-nat/mulle-sde/archive/0.1.0.tar.gz"
-  sha256 "21f2d7f304abca700eca506d8c621cc688d3dad6a44e96b3800418b0c44d258b"
-  # version "0.1.0"
+  url "https://github.com/mulle-nat/mulle-sde/archive/0.1.1.tar.gz"
+  sha256 "3a82b824bdbdc5ebaec52bcc9d5d5b333bb19def9f9bd17d16a2864c9300869b"
+  # version "0.1.1"
 
   depends_on "mulle-kybernetik/software/mulle-bootstrap"
 
