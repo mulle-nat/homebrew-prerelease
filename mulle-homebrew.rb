46257aa3454760fc95437ecc21ea4ee756b7866e
Nat! <nat@mulle-kybernetik.com>
2017-09-20 12:12:08 +0200

    5.2.1 release of mulle-homebrew

--- mulle-homebrew.rb
+++ mulle-homebrew.rb
@@ -1,9 +1,9 @@
 class MulleHomebrew < Formula
   desc "mulle-homebrew is ..."
   homepage "https://github.com/mulle-nat/mulle-homebrew"
-  url "https://github.com/mulle-nat/mulle-homebrew/archive/5.2.0.tar.gz"
-  sha256 "7b18b67786639d953d89b48bd56cb492d6933532f88c74b4a796403048f43bc9"
-  # version "5.2.0"
+  url "https://github.com/mulle-nat/mulle-homebrew/archive/5.2.1.tar.gz"
+  sha256 "d33d80449146a372428f18b139f400ef4b871526b03e379c705adf93e61f2d65"
+  # version "5.2.1"
 
   depends_on "mulle-kybernetik/software/mulle-build" => :build
   depends_on "mulle-kybernetik/software/mulle-bootstrap" => :build
