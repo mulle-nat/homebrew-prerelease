678a63ad0e7604fcc52ba9c48e0b42da2b9dcfd8
Nat! <nat@mulle-kybernetik.com>
2017-09-20 14:03:27 +0200

    1.1.9 release of mulle-utf

--- mulle-utf.rb
+++ mulle-utf.rb
@@ -1,9 +1,9 @@
 class MulleUtf < Formula
   desc "UTF8-16-32 analysis, conversion, classification library"
   homepage "https://github.com/mulle-nat/mulle-utf"
-  url "https://github.com/mulle-nat/mulle-utf/archive/1.1.7.tar.gz"
-  sha256 "8c72e12f218a46344d6d8254402293cf996102792a720328d168589605bd457d"
-  # version "1.1.7"
+  url "https://github.com/mulle-nat/mulle-utf/archive/1.1.9.tar.gz"
+  sha256 "f890d2c00a53723827f3973fd8bc65303a8fae8e87a7c8f5ed80207f62258725"
+  # version "1.1.9"
 
   depends_on "mulle-kybernetik/software/mulle-allocator"
 
