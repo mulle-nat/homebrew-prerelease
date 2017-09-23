3215e78b3cda73894081086e071962e80cc15e4e
Nat! <nat@mulle-kybernetik.com>
2017-09-23 20:29:48 +0200

    3.12.3 release of mulle-build

--- mulle-build.rb
+++ mulle-build.rb
@@ -1,9 +1,9 @@
 class MulleBuild < Formula
   desc "🔨 Build and install tool on top of cmake and mulle-bootstrap"
   homepage "https://github.com/mulle-nat/mulle-build"
-  url "https://github.com/mulle-nat/mulle-build/archive/3.12.2.tar.gz"
-  sha256 "cba96e4cc59e95d7a55fa82d11e5f595a24e0d06ced4fdd77a78e72072d85530"
-  # version "3.12.2"
+  url "https://github.com/mulle-nat/mulle-build/archive/3.12.3.tar.gz"
+  sha256 "2ef6d6c81c983400176e6c1c7b3ea3d0871496022174202eab288c7143248a25"
+  # version "3.12.3"
 
   depends_on "mulle-kybernetik/software/mulle-bootstrap"
   def install
