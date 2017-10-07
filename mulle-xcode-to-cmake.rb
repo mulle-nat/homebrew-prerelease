3aaccfce634329248e9e96d4de4f15450c763918
Nat! <nat@mulle-kybernetik.com>
2017-10-07 18:21:17 +0200

    0.6.6 release of mulle-xcode-to-cmake

--- mulle-xcode-to-cmake.rb
+++ mulle-xcode-to-cmake.rb
@@ -2,20 +2,13 @@ class MulleXcodeToCmake < Formula
   desc "mulle-xcode-to-cmake is ..."
   homepage "https://github.com/mulle-nat/mulle-xcode-to-cmake"
   url "https://github.com/mulle-nat/mulle-xcode-to-cmake/archive/0.6.6.tar.gz"
-  sha256 "8093bb1aa39c6f88c259182e7c4e10ed6005f9e89376c1afcb08469873c4d88d"
+  sha256 "782aafe2a33f218231dec1e5f78ce73a2dec84e04f5bfe218421370dfea6bff3"
   # version "0.6.6"
 
   depends_on "mulle-kybernetik/software/mulle-build" => :build
   depends_on "mulle-kybernetik/software/mulle-bootstrap" => :build
-
-  def install
-    system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
-  end
-
-  test do
-    if File.directory? 'tests'
-      system "mulle-test", "-vvv", "--fast-test"
-    end
-  end
+   def install
+      system "xcodebuild", "DSTROOT=#{prefix}", "INSTALL_PATH=/bin", "install"
+   end
 end
 # FORMULA mulle-xcode-to-cmake.rb
