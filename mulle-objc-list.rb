77f89e7fb83f212b5d3e93777d598580e83a82aa
Nat! <nat@mulle-kybernetik.com>
2017-10-08 20:46:05 +0200

    0.12.1 release of mulle-objc-list

--- mulle-objc-list.rb
+++ mulle-objc-list.rb
@@ -2,7 +2,7 @@ class MulleObjcList < Formula
   desc "mulle-objc-list list classes and categories or methods of a library"
   homepage "https://github.com/mulle-nat/mulle-objc-list"
   url "https://github.com/mulle-nat/mulle-objc-list/archive/0.12.1.tar.gz"
-  sha256 "3836dca1c6cff3906a38c274fc7e9cb3c6950e1753b85d0b4c3825965594d9d0"
+  sha256 "0f5e03192fcaef9e08dd39f291e377f2cf073ef5bea87d17841632ecdf290536"
   # version "0.12.1"
 
   depends_on "mulle-kybernetik/software/mulle-objc-runtime"
