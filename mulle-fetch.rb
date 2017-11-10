24316a05a6da19d56e9b4ee82866d31fcf5b0f50
Nat! <nat@codeon.de>
2017-11-10 16:26:36 +0100

    1.1.0 release of mulle-fetch

--- mulle-fetch.rb
+++ mulle-fetch.rb
@@ -2,7 +2,7 @@ class MulleFetch < Formula
   desc "🏃 Retrieve project archives or repostiories in various shapes and forms"
   homepage "https://github.com/mulle-nat/mulle-fetch"
   url "https://github.com/mulle-nat/mulle-fetch/archive/1.1.0.tar.gz"
-  sha256 "2fbde78fca3f3c8125d1272ccf92c9e9280d8a8f511c7e6e876c786f3b62bffd"
+  sha256 "e99cacdc6081d44713912097f9cd49fc4bba5235584ebab9028f624bca640e8e"
   # version "1.1.0"
 
   depends_on "mulle-kybernetik/software/mulle-bashfunctions"
