067fedac33c58681e178cd90945393d545f13dc1
Nat! <nat@mulle-kybernetik.com>
2017-12-22 00:31:11 +0100

    0.1.0 release of mulle-make

--- mulle-make.rb
+++ mulle-make.rb
@@ -1,9 +1,9 @@
 class MulleMake < Formula
 desc "🤖 Build projects using a variety of build systems"
 homepage "https://github.com/mulle-nat/mulle-make"
-url "https://github.com/mulle-nat/mulle-make/archive/1.0.0.tar.gz"
-sha256 "bb2b9b38ff231b88aa723ad0099da9225c44ffdf35b58f2846a91de0f49ccd0c"
-# version "1.0.0"
+url "https://github.com/mulle-nat/mulle-make/archive/0.1.0.tar.gz"
+sha256 "a7c44be5bacacbd72ba6af76aa884d8057929742248433ac23077f01aa1cab6b"
+# version "0.1.0"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 depends_on "cmake"
