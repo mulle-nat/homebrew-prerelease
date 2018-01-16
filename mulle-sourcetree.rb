e610e9e7d960b2c595dc4bf2b1a0b0693b6cdf34
Nat! <nat@mulle-kybernetik.com>
2018-01-16 22:39:38 +0100

    0.3.1 release of mulle-sourcetree

--- mulle-sourcetree.rb
+++ mulle-sourcetree.rb
@@ -1,9 +1,9 @@
 class MulleSourcetree < Formula
 desc "🌲 Project composition and maintainance with build support"
 homepage "https://github.com/mulle-nat/mulle-sourcetree"
-url "https://github.com/mulle-nat/mulle-sourcetree/archive/0.3.0.tar.gz"
+url "https://github.com/mulle-nat/mulle-sourcetree/archive/0.3.1.tar.gz"
 sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
-# version "0.3.0"
+# version "0.3.1"
 
 depends_on "mulle-kybernetik/software/mulle-fetch"
 def install
