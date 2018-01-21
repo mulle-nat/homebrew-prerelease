b947b0244313155e811c4d9f5787c10779a5a3af
Nat! <nat@mulle-kybernetik.com>
2018-01-21 21:46:39 +0100

    1.2.3 release of mulle-bashfunctions

--- mulle-bashfunctions.rb
+++ mulle-bashfunctions.rb
@@ -1,9 +1,9 @@
 class MulleBashfunctions < Formula
 desc "🥊 A versioned collection of bash functions"
 homepage "https://github.com/mulle-nat/mulle-bashfunctions"
-url "https://github.com/mulle-nat/mulle-bashfunctions/archive/1.2.2.tar.gz"
-sha256 "f67395a7526521f1855463d22d2ece7e164e4588e32bfd0af16a730113bbadfd"
-# version "1.2.2"
+url "https://github.com/mulle-nat/mulle-bashfunctions/archive/1.2.3.tar.gz"
+sha256 "8f6a4fae5982ee81d2466b774d5366980bd76dcf57f0babb6dccccb8a4092cc4"
+# version "1.2.3"
 def install
   system "./install.sh", "#{prefix}"
 end
