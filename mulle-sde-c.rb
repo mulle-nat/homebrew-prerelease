1f5e2849798487508df69b8073bbbfe16a4c88b3
Nat! <nat@mulle-kybernetik.com>
2018-02-20 22:34:01 +0100

    0.3.0 release of mulle-sde-c

--- mulle-sde-c.rb
+++ mulle-sde-c.rb
@@ -1,9 +1,9 @@
 class MulleSdeC < Formula
 desc "🏋🏼 mulle-sde, c support"
 homepage "https://github.com/mulle-nat/mulle-sde-c"
-url "https://github.com/mulle-nat/mulle-sde-c/archive/0.1.0.tar.gz"
+url "https://github.com/mulle-nat/mulle-sde-c/archive/0.3.0.tar.gz"
 sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-# version "0.1.0"
+# version "0.3.0"
 
 depends_on "mulle-kybernetik/software/mulle-sde"
 depends_on "cmake"
