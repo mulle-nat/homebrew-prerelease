36eef0b81688eb5f8a6aa51f89a86c35913ed96e
Nat! <nat@mulle-kybernetik.com>
2018-03-08 00:55:24 +0100

    0.0.4 release of mulle-match

--- mulle-match.rb
+++ mulle-match.rb
@@ -1,9 +1,9 @@
 class MulleMatch < Formula
 desc "🕵🏻‍ Extensible filesystem observation with callbacks"
 homepage "https://github.com/mulle-sde/mulle-match"
-url "https://github.com/mulle-sde/mulle-match/archive/0.0.3.tar.gz"
-sha256 "92af954ffc7265003f0a8e1659308f6c20675956414e921d8750b46b1adf60a0"
-# version "0.0.3"
+url "https://github.com/mulle-sde/mulle-match/archive/0.0.4.tar.gz"
+sha256 "ae951826caa9f30b476235c87f604c996cfa14e424e6b19ec37bb0fdf9ebf1e8"
+# version "0.0.4"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
