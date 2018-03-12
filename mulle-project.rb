b317da5beb5fe7d08ce3b77588941d85738e0f3c
Nat! <nat@mulle-kybernetik.com>
2018-03-12 13:24:42 +0100

    1.2.1 release of mulle-project

--- mulle-project.rb
+++ mulle-project.rb
@@ -1,9 +1,9 @@
 class MulleProject < Formula
 desc "🤷🏾‍♀️ Manage project versions and releases"
 homepage "https://github.com/mulle-nat/mulle-project"
-url "https://github.com/mulle-nat/mulle-project/archive/1.2.0.tar.gz"
-sha256 "41a8c131d433cd27ff5dad4e1e453b587d407714c8bd81d8964474141a89b1dc"
-# version "1.2.0"
+url "https://github.com/mulle-nat/mulle-project/archive/1.2.1.tar.gz"
+sha256 "2620ccf65bd786e7e7040e7cf4960f39dbfa0061dde5a41e3170ad3630e33fac"
+# version "1.2.1"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
