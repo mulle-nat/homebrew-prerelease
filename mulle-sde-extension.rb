da1b3df7c83485ddddfb2b390dc8d0849ea3d088
Nat! <nat@mulle-kybernetik.com>
2018-03-14 01:25:11 +0100

    0.0.3 release of mulle-sde-extension

--- mulle-sde-extension.rb
+++ mulle-sde-extension.rb
@@ -1,12 +1,11 @@
 class MulleSdeExtension < Formula
-desc "🏋🏼 A mulle-sde extension to write and install and package mulle-sde extensions with cmake"
+desc "🏋🏼 A mulle-sde extension to write mulle-sde extensions"
 homepage "https://github.com/mulle-nat/mulle-sde-extension"
-url "https://github.com/mulle-nat/mulle-sde-extension/archive/0.0.5.tar.gz"
+url "https://github.com/mulle-nat/mulle-sde-extension/archive/0.0.3.tar.gz"
 sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-# version "0.0.5"
+# version "0.0.3"
 
-depends_on "mulle-kybernetik/software/mulle-sde-extension"
-depends_on "mulle-kybernetik/software/mulle-sde-cmake"
+depends_on "mulle-kybernetik/software/mulle-sde"
 def install
   system "./install.sh", "#{prefix}"
 end
