509fb797dffbd465178114a5f5c36866fa4a68bb
Nat! <nat@mulle-kybernetik.com>
2018-05-01 01:09:45 +0200

    0.4.5 release of mulle-monitor

--- mulle-monitor.rb
+++ mulle-monitor.rb
@@ -1,9 +1,9 @@
 class MulleMonitor < Formula
 desc "🕵🏻‍ Extensible filesystem observation with callbacks"
 homepage "https://github.com/mulle-sde/mulle-monitor"
-url "https://github.com/mulle-sde/mulle-monitor/archive/0.4.4.tar.gz"
-sha256 "dd9a2d9b59a976b7ca6b4377f814810d0fdfb693e980e0b2a85475fca3fa31a5"
-# version "0.4.4"
+url "https://github.com/mulle-sde/mulle-monitor/archive/0.4.5.tar.gz"
+sha256 "dc5c2cb43548d49feb017a7c9d19ab3e1c588c0b8eb55f86942c371a32533ac4"
+# version "0.4.5"
 
 depends_on "mulle-kybernetik/software/mulle-fetch"
 depends_on "fswatch"
