a7021d887598dc60f09299d1ef9cacacc5e27c63
Nat! <nat-tv@mulle-kybernetik.com>
2018-05-25 00:32:32 +0200

    1.3.14 release of mulle-fetch

--- mulle-fetch.rb
+++ mulle-fetch.rb
@@ -1,13 +1,13 @@
 class MulleFetch < Formula
 desc "🏃🏿 Download and unpack source repositories or archives"
 homepage "https://github.com/mulle-nat/mulle-fetch"
-url "https://github.com/mulle-nat/mulle-fetch/archive/1.3.13.tar.gz"
+url "https://github.com/mulle-nat/mulle-fetch/archive/1.3.14.tar.gz"
 sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-# version "1.3.13"
+# version "1.3.14"
 
 depends_on "mulle-bashfunctions"
 def install
-  system "./install.sh", "#{prefix}"
+  system "./installer", "#{prefix}"
 end
 end
 # FORMULA mulle-fetch.rb
