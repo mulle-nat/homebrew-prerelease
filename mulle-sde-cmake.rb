bb3ddefc5697221020e15cbf56c9c02729d4b6b2
Nat! <nat-tv@mulle-kybernetik.com>
2018-05-25 00:36:27 +0200

    0.6.5 release of mulle-sde-cmake

--- mulle-sde-cmake.rb
+++ mulle-sde-cmake.rb
@@ -1,14 +1,14 @@
 class MulleSdeCmake < Formula
 desc "🏋🏼 mulle-sde, cmake support"
 homepage "https://github.com/mulle-nat/mulle-sde-cmake"
-url "https://github.com/mulle-nat/mulle-sde-cmake/archive/0.6.4.tar.gz"
+url "https://github.com/mulle-nat/mulle-sde-cmake/archive/0.6.5.tar.gz"
 sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-# version "0.6.4"
+# version "0.6.5"
 
 depends_on "mulle-kybernetik/software/mulle-sde"
 depends_on "cmake"
 def install
-  system "./install.sh", "#{prefix}"
+  system "./installer", "#{prefix}"
 end
 end
 # FORMULA mulle-sde-cmake.rb
