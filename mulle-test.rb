c8bba7ed0a29eb91957eb84185e92c43d4ce0c93
Nat! <nat@codeon.de>
2018-05-25 12:50:50 +0200

    3.0.7 release of mulle-test

--- mulle-test.rb
+++ mulle-test.rb
@@ -1,9 +1,11 @@
 class MulleTest < Formula
 desc "☑︎ Run tests and compare their results against expectations"
 homepage "https://github.com/mulle-nat/mulle-test"
-url "https://github.com/mulle-nat/mulle-test/archive/3.0.6.tar.gz"
+url "https://github.com/mulle-nat/mulle-test/archive/3.0.7.tar.gz"
 sha256 "395408a3dc9c3db2b5c200b8722a13a60898c861633b99e6e250186adffd1370"
-# version "3.0.6"
+# version "3.0.7"
+
+depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
   system "./install.sh", "#{prefix}"
 end
