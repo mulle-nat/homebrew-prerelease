59ac953ff250dcef431ec9dfa5decb2d723a75f0
Nat! <nat@mulle-kybernetik.com>
2018-05-26 12:43:18 +0200

    0.4.11 release of mulle-make

--- mulle-make.rb
+++ mulle-make.rb
@@ -1,9 +1,9 @@
 class MulleMake < Formula
 desc "💄 Build projects with tools like cmake, meson, autoconf"
 homepage "https://github.com/mulle-nat/mulle-make"
-url "https://github.com/mulle-nat/mulle-make/archive/0.4.10.tar.gz"
+url "https://github.com/mulle-nat/mulle-make/archive/0.4.11.tar.gz"
 sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-# version "0.4.10"
+# version "0.4.11"
 
 depends_on "mulle-bashfunctions"
 def install
