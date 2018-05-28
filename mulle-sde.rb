28d62507b5ec97757665af7825821ae0f7114500
Nat! <nat@codeon.de>
2018-05-28 12:36:49 +0200

    0.18.4 release of mulle-sde

--- mulle-sde.rb
+++ mulle-sde.rb
@@ -1,9 +1,9 @@
 class MulleSde < Formula
 desc "🏋🏼 Cross-platform IDE for the command-line"
-homepage "https://github.com/mulle-nat/mulle-sde"
-url "https://github.com/mulle-nat/mulle-sde/archive/0.18.3.tar.gz"
-sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-# version "0.18.3"
+homepage "https://github.com/mulle-sde/mulle-sde"
+url "https://github.com/mulle-sde/mulle-sde/archive/0.18.4.tar.gz"
+sha256 "99316bde7aff67b6337474de513cf8e3da00fd7598633fd32498cda1d24f1125"
+# version "0.18.4"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 depends_on "mulle-kybernetik/software/mulle-env"
