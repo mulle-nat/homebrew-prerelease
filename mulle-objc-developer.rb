e3efee6e9cc4639c10ea9054a8c15c6bb3726e46
Nat! <nat@mulle-kybernetik.com>
2018-06-16 00:09:56 +0200

    0.13.0 release of mulle-objc-developer

--- mulle-objc-developer.rb
+++ mulle-objc-developer.rb
@@ -1,9 +1,9 @@
 class MulleObjcDeveloper < Formula
 desc " mulle-sde installer + extensions for mulle-obj development"
 homepage "https://github.com/mulle-objc/mulle-objc-developer"
-url "https://github.com/mulle-objc/mulle-objc-developer/archive/0.13.2.tar.gz"
-sha256 "e5858977212c5a2f3665771bef341d12ff6a6cd757b6bfc5d4504293c7e65208"
-# version "0.13.2"
+url "https://github.com/mulle-objc/mulle-objc-developer/archive/0.13.0.tar.gz"
+sha256 "2882b4a62ef04938c74a52b53f9c92c1f68f16ac02fe36819755b401f42698f6"
+# version "0.13.0"
 
 depends_on "mulle-kybernetik/software/mulle-c-developer"
 depends_on "codeon-gmbh/software/mulle-clang"
