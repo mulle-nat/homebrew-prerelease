f9481079f05d48046419289c3e06bf67e4e16d3d
Nat! <nat@mulle-kybernetik.com>
2018-07-09 23:09:19 +0200

    0.8.0 release of mulle-craft

--- mulle-craft.rb
+++ mulle-craft.rb
@@ -1,9 +1,9 @@
 class MulleCraft < Formula
 desc "🚬 Build projects using mulle-make and mulle-sourcetre"
 homepage "https://github.com/mulle-sde/mulle-craft"
-url "https://github.com/mulle-sde/mulle-craft/archive/0.7.3.tar.gz"
-sha256 "4c025167dbcc6ee68085f65d3a1aee4c3259f216e5956cc15fb58514e4db206a"
-# version "0.7.3"
+url "https://github.com/mulle-sde/mulle-craft/archive/0.8.0.tar.gz"
+sha256 "7f4da70d337639ee6e4792771bfb9b01e0fb411b7faba17a162d790d50124b32"
+# version "0.8.0"
 
 depends_on "mulle-kybernetik/software/mulle-sourcetree"
 depends_on "mulle-kybernetik/software/mulle-make"
