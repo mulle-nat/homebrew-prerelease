cb9cc466012ee116df087a18258c73b61dd067f5
Nat! <nat@mulle-kybernetik.com>
2018-07-13 14:00:43 +0200

    0.13.0 release of mulle-foundation-developer

--- mulle-foundation-developer.rb
+++ mulle-foundation-developer.rb
@@ -1,25 +1,13 @@
 class MulleFoundationDeveloper < Formula
-  desc "🎁 Develop Objective-C with the MulleFoundation using mulle-objc"
-  homepage "https://github.com/mulle-nat/mulle-foundation-developer"
-  url "https://github.com/mulle-nat/mulle-foundation-developer/archive/0.12.1.tar.gz"
-  sha256 "d26e85f6517812dad8ff0ab7057a482d9d73621b8a6e7a11772edf07ecde828c"
-  # version "0.12.1"
+desc "👑 Objective-C development with the MulleFoundation and mulle-sde"
+homepage "https://github.com/MulleFoundation/mulle-foundation-developer"
+url "https://github.com/MulleFoundation/mulle-foundation-developer/archive/0.13.0.tar.gz"
+sha256 "203f267b52bebfabc742827ee8a58b682c44102fd26368ba2b24eca91d1398c4"
+# version "0.13.0"
 
-  depends_on "mulle-objc/software/mulle-objc-developer"
-
-  depends_on "mulle-kybernetik/software/mulle-build" => :build
-  depends_on "mulle-kybernetik/software/mulle-bootstrap" => :build
-  depends_on "cmake" => :build
-  def install
-    system "mulle-install", "-vvv", "--prefix", prefix, "--homebrew"
-    ohai "Get started with a \"hello world\" example:"
-    ohai "    mulle-foundation-init -d hello executable"
-  end
-
-  test do
-    if File.directory? 'tests'
-      system "mulle-test", "-vvv", "--fast-test"
-    end
-  end
+depends_on "mulle-kybernetik/software/mulle-objc-developer"
+def install
+  system "./installer", "#{prefix}"
+end
 end
 # FORMULA mulle-foundation-developer.rb
