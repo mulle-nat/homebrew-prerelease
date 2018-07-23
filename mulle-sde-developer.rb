afa52250d2895e24da2cdc25851655f79a7ad24a
Nat! <nat@codeon.de>
2018-07-23 17:54:52 +0200

    0.9.8 release of mulle-sde-developer

--- mulle-sde-developer.rb
+++ mulle-sde-developer.rb
@@ -1,9 +1,9 @@
 class MulleSdeDeveloper < Formula
 desc "🏋🏼 mulle-sde with C, cmake and mulle-sde extension support"
 homepage "https://github.com/mulle-sde/mulle-sde-developer"
-url "https://github.com/mulle-sde/mulle-sde-developer/archive/0.9.7.tar.gz"
-sha256 "9f81374b577e2501470ca77894f08f5f022e73e4e50007976a9f43e1d750ef33"
-# version "0.9.7"
+url "https://github.com/mulle-sde/mulle-sde-developer/archive/0.9.8.tar.gz"
+sha256 "5b9d0acfceec1491f2176ec78c4f49ceffba90f4edeaf7791a42376100209c78"
+# version "0.9.8"
 
 depends_on "mulle-kybernetik/software/mulle-sde"
 depends_on "cmake"
