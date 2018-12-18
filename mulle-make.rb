e4a710c2519d480b55456cf64370e7f06f081a6c
Nat! <nat-tv@mulle-kybernetik.com>
2018-12-18 16:05:56 +0100

    0.7.0 release of mulle-make

--- mulle-make.rb
+++ mulle-make.rb
@@ -1,13 +1,13 @@
 class MulleMake < Formula
 desc "💄 Build projects with tools like cmake, meson, autoconf"
 homepage "https://github.com/mulle-sde/mulle-make"
-url "https://github.com/mulle-sde/mulle-make/archive/0.6.1.tar.gz"
-sha256 "4c248195dbfb1fdf8d1385dae4a311cffb2a97fbd4aae5b50e7720d2a8cdc017"
-# version "0.6.1"
+url "https://github.com/mulle-sde/mulle-make/archive/0.7.0.tar.gz"
+sha256 "5ac1ca342987557285dc8e769f46ecac231f7deb60ab47bc0e7f52663c8d1a1f"
+# version "0.7.0"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
-  system "./installer", "#{prefix}"
+  system "./bin/installer", "#{prefix}"
 end
 end
 # FORMULA mulle-make.rb
