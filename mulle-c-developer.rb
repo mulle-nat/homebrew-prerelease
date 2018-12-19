fda5853b500488937c4e1b256e5a94a3daad52e7
Nat! <nat@codeon.de>
2018-12-19 17:15:15 +0100

    0.9.2 release of mulle-c-developer

--- mulle-c-developer.rb
+++ mulle-c-developer.rb
@@ -1,9 +1,9 @@
 class MulleCDeveloper < Formula
 desc "🧢 mulle-c developer kit for mulle-sde"
 homepage "https://github.com/mulle-c/mulle-c-developer"
-url "https://github.com/mulle-c/mulle-c-developer/archive/0.9.1.tar.gz"
-sha256 "f940a5944c6cca404b8576343dd3a7bd66b76352b7091a0d13a375d23cac2355"
-# version "0.9.1"
+url "https://github.com/mulle-c/mulle-c-developer/archive/0.9.2.tar.gz"
+sha256 "ed8c18182a8e262d313c26a17f412b09dbb6ec92fe6e34f5d533e8cbc120a63f"
+# version "0.9.2"
 
 depends_on "mulle-kybernetik/software/mulle-sde-developer"
 def install
