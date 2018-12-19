2a942f3bfc4b31b96e69cff73e1317106fd6c3f9
Nat! <nat@codeon.de>
2018-12-19 11:59:13 +0100

    4.0.1 release of mulle-test

--- mulle-test.rb
+++ mulle-test.rb
@@ -1,12 +1,12 @@
 class MulleTest < Formula
 desc "☑︎ Run tests and compare their results against expectations"
 homepage "https://github.com/mulle-sde/mulle-test"
-url "https://github.com/mulle-sde/mulle-test/archive/4.0.0.tar.gz"
-sha256 "9e1e0d746f18496f79fbc8d0509a43d7474be834db830449c6a3729082e7d445"
-# version "4.0.0"
+url "https://github.com/mulle-sde/mulle-test/archive/4.0.1.tar.gz"
+sha256 "72cc0ce6b6bfa48ab079ca14b1d7b537d5d6bb72df4067f0237709228f8ac3d8"
+# version "4.0.1"
 
-depends_on "mulle-sde/mulle-sde"
-depends_on "mulle-sde/mulle-platform"
+depends_on "mulle-kybernetik/mulle-sde"
+depends_on "mulle-kybernetik/mulle-platform"
 def install
   system "./install.sh", "#{prefix}"
 end
