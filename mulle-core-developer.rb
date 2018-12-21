99753761ca9e3448e6fa23c20757f258a5419b05
Nat! <nat@codeon.de>
2018-12-21 13:10:58 +0100

    0.0.3 release of mulle-core-developer

--- mulle-core-developer.rb
+++ mulle-core-developer.rb
@@ -1,9 +1,9 @@
 class MulleCoreDeveloper < Formula
 desc "🧢 mulle-core developer kit for mulle-sde"
 homepage "https://github.com/mulle-core/mulle-core-developer"
-url "https://github.com/mulle-core/mulle-core-developer/archive/0.0.2.tar.gz"
-sha256 "05a17124435f45a616d074cbd810163067c7bfd66499400d43d2e824880d690c"
-# version "0.0.2"
+url "https://github.com/mulle-core/mulle-core-developer/archive/0.0.3.tar.gz"
+sha256 "a80a7898569cde7dcb57c82120170a73a85feeb001fdf1f45fba92fa205e519c"
+# version "0.0.3"
 
 depends_on "mulle-kybernetik/software/mulle-c-developer"
 def install
