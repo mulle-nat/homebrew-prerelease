7e0cee8f61d35d2647c73fb49db5795d003d6550
Nat! <nat@mulle-kybernetik.com>
2018-12-22 01:31:10 +0100

    1.6.2 release of mulle-fetch

--- mulle-fetch.rb
+++ mulle-fetch.rb
@@ -1,9 +1,9 @@
 class MulleFetch < Formula
 desc "🏃🏿 Download and unpack source repositories or archives"
 homepage "https://github.com/mulle-sde/mulle-fetch"
-url "https://github.com/mulle-sde/mulle-fetch/archive/1.6.1.tar.gz"
-sha256 "1dea23e5900b33ffe39df9e1ca681d1343a83668c835e2ff46d4259c6f7022e4"
-# version "1.6.1"
+url "https://github.com/mulle-sde/mulle-fetch/archive/1.6.2.tar.gz"
+sha256 "3f8895e7fa88a7257638d4c9f93dcd2e2e0e4ba0cbca3976098f0b4fbe7e172e"
+# version "1.6.2"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
