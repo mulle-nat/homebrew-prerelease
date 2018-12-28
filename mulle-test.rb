491b8254ba3d17f16524791ebab8626101325f67
Nat! <nat@mulle-kybernetik.com>
2018-12-28 19:22:04 +0100

    4.0.9 release of mulle-test

--- mulle-test.rb
+++ mulle-test.rb
@@ -1,9 +1,9 @@
 class MulleTest < Formula
 desc "☑︎ Run tests and compare their results against expectations"
 homepage "https://github.com/mulle-sde/mulle-test"
-url "https://github.com/mulle-sde/mulle-test/archive/4.0.8.tar.gz"
-sha256 "43975307177637eaa2b3754daabd5de3148acb1bd9e2870b4939618349c46247"
-# version "4.0.8"
+url "https://github.com/mulle-sde/mulle-test/archive/4.0.9.tar.gz"
+sha256 "2466db2c871d54ec521d56677085ab5ffd24fd33ac75cd2fe42f93ac99a1eb41"
+# version "4.0.9"
 
 depends_on "mulle-kybernetik/software/mulle-sde"
 depends_on "mulle-kybernetik/software/mulle-platform"
