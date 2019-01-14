9430e86bcb7039464238f1184bfbdedc6f714fdc
Nat! <nat@mulle-kybernetik.com>
2019-01-15 00:26:30 +0100

    0.0.4 release of mulle-platform

--- mulle-platform.rb
+++ mulle-platform.rb
@@ -1,9 +1,9 @@
 class MullePlatform < Formula
 desc "👠 Query platform specifica and search for libraries"
 homepage "https://github.com/mulle-sde/mulle-platform"
-url "https://github.com/mulle-sde/mulle-platform/archive/0.0.3.tar.gz"
-sha256 "60b35052e38967cd24b71a66342c18ff5ad9a0ef647e208830ac655ae2df9e22"
-# version "0.0.3"
+url "https://github.com/mulle-sde/mulle-platform/archive/0.0.4.tar.gz"
+sha256 "eb08f1229b927e40cdb565046ff76bc94ca89c84b4e57399a2240455fe62a84c"
+# version "0.0.4"
 def install
   system "./bin/installer", "#{prefix}"
 end
