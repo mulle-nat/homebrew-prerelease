de5aa2d010ae740a7f8912f86d19ddedd09d2f21
Nat! <nat@mulle-kybernetik.com>
2019-01-15 00:22:30 +0100

    0.4.0 release of mulle-match

--- mulle-match.rb
+++ mulle-match.rb
@@ -1,9 +1,9 @@
 class MulleMatch < Formula
 desc "💕 Match filenames against a set of .gitignore like patternfiles"
 homepage "https://github.com/mulle-sde/mulle-match"
-url "https://github.com/mulle-sde/mulle-match/archive/0.3.2.tar.gz"
-sha256 "6b35db5d461fbfee61c221566aed562c12f8c95aafedc2d7b137870c1a1148da"
-# version "0.3.2"
+url "https://github.com/mulle-sde/mulle-match/archive/0.4.0.tar.gz"
+sha256 "bdf85a8ba71f976fba2b132f8f1ea970c7ab18d659c10da14858c3f6651bdadf"
+# version "0.4.0"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
