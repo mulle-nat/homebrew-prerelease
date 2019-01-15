c42626843a64c9247af416f294e2924f140cec59
Nat! <nat@mulle-kybernetik.com>
2019-01-15 20:16:53 +0100

    0.0.8 release of mulle-core-developer

--- mulle-core-developer.rb
+++ mulle-core-developer.rb
@@ -1,9 +1,9 @@
 class MulleCoreDeveloper < Formula
 desc "🧢 mulle-core developer kit for mulle-sde"
 homepage "https://github.com/mulle-core/mulle-core-developer"
-url "https://github.com/mulle-core/mulle-core-developer/archive/0.0.7.tar.gz"
-sha256 "79fb1f9e28bfa38b66566a5e4013a2065de4ac31c0930bc44ae67d4f85024107"
-# version "0.0.7"
+url "https://github.com/mulle-core/mulle-core-developer/archive/0.0.8.tar.gz"
+sha256 "939f77d7450b7eb758cb5e73b63b64d8e61afac5dff616e015b3c419d315175b"
+# version "0.0.8"
 
 depends_on "mulle-kybernetik/software/mulle-c-developer"
 def install
