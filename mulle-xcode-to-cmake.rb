41b37610a421f0b77b06a435add98b3c2c6b40dc
Nat! <nat@codeon.de>
2019-02-14 15:38:56 +0100

    0.7.1 release of mulle-xcode-to-cmake

--- mulle-xcode-to-cmake.rb
+++ mulle-xcode-to-cmake.rb
@@ -1,9 +1,9 @@
 class MulleXcodeToCmake < Formula
 desc "🎭 Convert Xcode projects to cmake CMakeLists.txt "
 homepage "https://github.com/mulle-nat/mulle-xcode-to-cmake"
-url "https://github.com/mulle-nat/mulle-xcode-to-cmake/archive/0.7.0.tar.gz"
-sha256 "0a910a5b04db33175a0a3abb3c4eac2699546b14aea1cbc30842b6b09363d310"
-# version "0.7.0"
+url "https://github.com/mulle-nat/mulle-xcode-to-cmake/archive/0.7.1.tar.gz"
+sha256 "094ee99bf3d84ebd3649af01fc145c15c72a1761cc92296c0c2628d959049b28"
+# version "0.7.1"
    def install
       system "xcodebuild", "DSTROOT=#{prefix}", "INSTALL_PATH=/bin", "install"
    end
