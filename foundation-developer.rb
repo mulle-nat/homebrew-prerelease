e3997da0c21c78195a05e2af861c18b12e131cfb
Nat! <nat@codeon.de>
2019-05-22 17:45:51 +0200

    0.14.11 release of foundation-developer

--- foundation-developer.rb
+++ foundation-developer.rb
@@ -1,9 +1,9 @@
 class FoundationDeveloper < Formula
 desc "👒 Objective C with mulle-sde and the mulle-objc Foundation"
 homepage "https://github.com/MulleFoundation/foundation-developer"
-url "https://github.com/MulleFoundation/foundation-developer/archive/0.14.10.tar.gz"
-sha256 "5c287a50af6abdeaaef168a3cbf8b4e400bc2c2dd4a098e0528f0414cca2332c"
-# version "0.14.10"
+url "https://github.com/MulleFoundation/foundation-developer/archive/0.14.11.tar.gz"
+sha256 "27cdc3c3e8a5491b2249a437a1bad7b77d8642d132d442ac26eaf0960b47adae"
+# version "0.14.11"
 
 depends_on "mulle-kybernetik/software/mulle-foundation-developer"
 def install
