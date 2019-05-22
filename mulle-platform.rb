dde5e1a05caf0ac0bfd2692c40e74e6371406fc7
Nat! <nat@codeon.de>
2019-05-22 15:34:05 +0200

    0.1.0 release of mulle-platform

--- mulle-platform.rb
+++ mulle-platform.rb
@@ -1,9 +1,9 @@
 class MullePlatform < Formula
 desc "👠 Query platform specifica and search for libraries"
 homepage "https://github.com/mulle-sde/mulle-platform"
-url "https://github.com/mulle-sde/mulle-platform/archive/0.0.4.tar.gz"
-sha256 "eb08f1229b927e40cdb565046ff76bc94ca89c84b4e57399a2240455fe62a84c"
-# version "0.0.4"
+url "https://github.com/mulle-sde/mulle-platform/archive/0.1.0.tar.gz"
+sha256 "323ccbf36f8d7f263c8580e1db862337b973e5c467233e5c3e2b6294554453f1"
+# version "0.1.0"
 def install
   system "./bin/installer", "#{prefix}"
 end
