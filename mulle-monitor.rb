c15305d42d908ca6b6d5b40fbe56f3cc5ce6353a
Nat! <nat@codeon.de>
2019-05-22 15:28:16 +0200

    0.7.2 release of mulle-monitor

--- mulle-monitor.rb
+++ mulle-monitor.rb
@@ -1,9 +1,9 @@
 class MulleMonitor < Formula
 desc "🕵🏻‍ Extensible filesystem observation with callbacks"
 homepage "https://github.com/mulle-sde/mulle-monitor"
-url "https://github.com/mulle-sde/mulle-monitor/archive/0.7.1.tar.gz"
-sha256 "58c2e84bd4cf9bd5f613dbe36f8cfd51aea6887500c1489ddc3e5c92d1123a95"
-# version "0.7.1"
+url "https://github.com/mulle-sde/mulle-monitor/archive/0.7.2.tar.gz"
+sha256 "9f41ec3ee53d53667c72402b5ece1a43fee39c71789f94227d7fef9fe5060cfc"
+# version "0.7.2"
 
 depends_on "mulle-kybernetik/software/mulle-match"
 depends_on "fswatch"
