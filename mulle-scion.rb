ef449b65b32293616d50aa01555ab9004790b642
Nat! <nat@codeon.de>
2019-10-22 13:21:18 +0200

    1859.1.1 release of mulle-scion

--- mulle-scion.rb
+++ mulle-scion.rb
@@ -1,15 +1,21 @@
 class MulleScion < Formula
 desc "A modern template engine for Objective C"
 homepage "https://github.com/mulle-nat/MulleScion"
-url "https://github.com/mulle-nat/MulleScion/archive/1859.0.0.tar.gz"
-sha256 "11a3475055e7bcec3192d5a683f51f7667cc58a8f0a707f49f87f5fa53db2f9f"
-# version "1859.0.0"
+url "https://github.com/mulle-nat/MulleScion/archive/1859.1.1.tar.gz"
+sha256 "d10c6d8a7235e4e07219b718bb959673422d71dd2d30262222f3f60ceba8b9a9"
+# version "1859.1.1"
 
 depends_on "mulle-build" => :build
 depends_on "mulle-bootstrap" => :build
 depends_on "cmake" => :build
-def install
-  system "xcodebuild", "-configuration", "Release", "DSTROOT=#{prefix}", "install"
-end
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
 end
 # FORMULA mulle-scion.rb
