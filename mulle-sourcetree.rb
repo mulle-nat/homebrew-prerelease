248969bc2091f9a8aaed07c73127217207ab20b5
Nat! <nat@mulle-kybernetik.com>
2020-03-16 12:54:57 +0100

    0.21.0 prerelease of mulle-sourcetree

--- mulle-sourcetree.rb
+++ mulle-sourcetree.rb
@@ -1,11 +1,11 @@
 class MulleSourcetree < Formula
 desc "🌲 Project composition and maintenance with build support"
 homepage "https://github.com/mulle-sde/mulle-sourcetree"
-url "https://github.com/mulle-sde/mulle-sourcetree/archive/0.20.1.tar.gz"
-sha256 "d9cb4a2826943234319a02e97fe15ef8be4ec66379593b5d502f5306a5adf1df"
-# version "0.20.1"
+url "https://github.com/mulle-sde/mulle-sourcetree/archive/0.21.0-prerelease.tar.gz"
+sha256 "1f2c27aafabe18169cd8bab8859f41159d1c18b226136a8eabaa7eea592ef309"
+# version "0.21.0"
 
-depends_on "mulle-kybernetik/software/mulle-fetch"
+depends_on "prerelease/prerelease/mulle-fetch"
 def install
   system "./bin/installer", "#{prefix}"
 end
