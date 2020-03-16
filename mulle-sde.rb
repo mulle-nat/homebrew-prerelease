278199157bacb5107b7a28aa1112f01f73442221
Nat! <nat@mulle-kybernetik.com>
2020-03-16 20:31:51 +0100

    0.40.0 prerelease of mulle-sde

--- mulle-sde.rb
+++ mulle-sde.rb
@@ -2,7 +2,7 @@ class MulleSde < Formula
 desc "💠 Cross-platform IDE for the command-line"
 homepage "https://github.com/mulle-sde/mulle-sde"
 url "https://github.com/mulle-sde/mulle-sde/archive/0.40.0-prerelease.tar.gz"
-sha256 "22ae80ca541c2b22785070c6dce18bc1ddf26821f5ebc532169a685c11d906e9"
+sha256 "35a0cfbf1a5f5f68285caed631e18c9988625ad9307e87cec1ced57daec7562b"
 # version "0.40.0"
 
 depends_on "prerelease/prerelease/mulle-bashfunctions"
