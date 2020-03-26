04575b0df1b01ef332d02f6d33231a723c80509d
Nat! <nat@codeon.de>
2020-03-26 16:46:31 +0100

    0.4.1 prerelease of mulle-platform

--- mulle-platform.rb
+++ mulle-platform.rb
@@ -1,9 +1,9 @@
 class MullePlatform < Formula
 desc "👠 Query platform specifica and search for libraries"
 homepage "https://github.com/mulle-sde/mulle-platform"
-url "https://github.com/mulle-sde/mulle-platform/archive/0.4.0-prerelease.tar.gz"
-sha256 "0dae8d85680fe738f07ef9ce600d4fd92c6875a5ec516279d32b6aefec29c014"
-# version "0.4.0"
+url "https://github.com/mulle-sde/mulle-platform/archive/0.4.1-prerelease.tar.gz"
+sha256 "e1675fee86f5e7101dea2ad18517164caedf04200673118b1395c0ffbb948f95"
+# version "0.4.1"
 def install
   system "./bin/installer", "#{prefix}"
 end
