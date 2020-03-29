f4235ba13909d6624b97331338632fcfa5854a52
Nat! <nat@mulle-kybernetik.com>
2020-03-29 21:58:16 +0200

    2.0.0 prerelease of mulle-dispense

--- mulle-dispense.rb
+++ mulle-dispense.rb
@@ -2,7 +2,7 @@ class MulleDispense < Formula
 desc " 🚰 Copy build products and reorganize them "
 homepage "https://github.com/mulle-sde/mulle-dispense"
 url "https://github.com/mulle-sde/mulle-dispense/archive/2.0.0-prerelease.tar.gz"
-sha256 "7f6662e3ce132a9fddd22a10fbfb8e38ba13f8c432edde05f36cda847ae35c3d"
+sha256 "d6b9406efb93f0886fde033fa57b8d93a4c0c4b13297977d1005b78c60cf5cd9"
 # version "2.0.0"
 
 depends_on "prerelease/prerelease/mulle-bashfunctions"
