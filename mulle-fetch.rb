594cfdf26ec572dd3542bfa3f9eaa62de3f5649d
Nat! <nat@mulle-kybernetik.com>
2020-03-29 21:59:36 +0200

    1.8.0 prerelease of mulle-fetch

--- mulle-fetch.rb
+++ mulle-fetch.rb
@@ -2,7 +2,7 @@ class MulleFetch < Formula
 desc "🏃🏿 Download and unpack source repositories or archives"
 homepage "https://github.com/mulle-sde/mulle-fetch"
 url "https://github.com/mulle-sde/mulle-fetch/archive/1.8.0-prerelease.tar.gz"
-sha256 "36a21113fe20ab76be57581862a927d1ad93d857131f7a80d595687f447d40cb"
+sha256 "45d4ddb3ec77b23c1d7941e9f7d1b776917eb584369e0308935d7bed4e861701"
 # version "1.8.0"
 
 depends_on "prerelease/prerelease/mulle-bashfunctions"
