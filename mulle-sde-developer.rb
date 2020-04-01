11e700952f128261ea59e0f1c739d00e8fa7311c
Nat! <nat@mulle-kybernetik.com>
2020-04-01 23:57:48 +0200

    0.18.0 prerelease of mulle-sde-developer

--- mulle-sde-developer.rb
+++ mulle-sde-developer.rb
@@ -2,7 +2,7 @@ class MulleSdeDeveloper < Formula
 desc "🎓 mulle-sde installer + various extensions for mulle-sde"
 homepage "https://github.com/mulle-sde/mulle-sde-developer"
 url "https://github.com/mulle-sde/mulle-sde-developer/archive/0.18.0-prerelease.tar.gz"
-sha256 "c8f42851078a2219e32937f43f9bd59d1681b982f48beda4d9feb8d780b170b7"
+sha256 "2140aea831bda26c3cce7c4775b7c98c5ec4293c6b634759e323d14b9363f4a4"
 # version "0.18.0"
 
 depends_on "prerelease/prerelease/mulle-sde"
