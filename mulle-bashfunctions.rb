503c8c49a7d2b8132bf276b31d7dad65608260f9
Nat! <nat@mulle-kybernetik.com>
2020-04-01 11:29:36 +0200

    3.0.1 prerelease of mulle-bashfunctions

--- mulle-bashfunctions.rb
+++ mulle-bashfunctions.rb
@@ -2,7 +2,7 @@ class MulleBashfunctions < Formula
 desc "🥊 A versioned collection of bash functions"
 homepage "https://github.com/mulle-nat/mulle-bashfunctions"
 url "https://github.com/mulle-nat/mulle-bashfunctions/archive/3.0.1-prerelease.tar.gz"
-sha256 "bfc10c493fb641a8034b6c973ce5f6f7f6983ac30fd040388ed1e5505cd65c55"
+sha256 "8f6400d4f5a43a99f330b080a1e8771973ca26b80fd591bb57bbd9e4eadd3aa4"
 # version "3.0.1"
 def install
   system "./bin/installer", "#{prefix}"
