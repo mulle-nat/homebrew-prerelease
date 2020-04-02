e1e1054965949d874dbb05c12c9ed13f120428ca
Nat! <nat@codeon.de>
2020-04-02 14:10:13 +0200

    0.18.0 prerelease of mulle-craft

--- mulle-craft.rb
+++ mulle-craft.rb
@@ -5,9 +5,9 @@ url "https://github.com/mulle-sde/mulle-craft/archive/0.18.0-prerelease.tar.gz"
 sha256 "fbd6868b49625525e5e0e7c85aa84da6343afaac588ef514b7d4ac74a3748835"
 # version "0.18.0"
 
-depends_on "mulle-kybernetik/prerelease/mulle-sourcetree"
-depends_on "mulle-kybernetik/prerelease/mulle-make"
-depends_on "mulle-kybernetik/prerelease/mulle-dispense"
+depends_on "mulle-nat/prerelease/mulle-sourcetree"
+depends_on "mulle-nat/prerelease/mulle-make"
+depends_on "mulle-nat/prerelease/mulle-dispense"
 def install
   system "./bin/installer", "#{prefix}"
 end
