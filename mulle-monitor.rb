4ed3e752cf60a54c5f8f7dba9b59837520b1c2f1
Nat! <nat@codeon.de>
2020-04-02 13:43:57 +0200

    0.9.0 prerelease of mulle-monitor

--- mulle-monitor.rb
+++ mulle-monitor.rb
@@ -2,10 +2,10 @@ class MulleMonitor < Formula
 desc "🕵🏻‍ Extensible filesystem observation with callbacks"
 homepage "https://github.com/mulle-sde/mulle-monitor"
 url "https://github.com/mulle-sde/mulle-monitor/archive/0.9.0-prerelease.tar.gz"
-sha256 "0f4d880038e1d0d7970ebacbc2f41842e11ca6d2038ce8aec8fc20eaee64ff25"
+sha256 "5a18f89483d94c0a85c20f4c8515316fbf7be152c6c077eb019cdefe1f8c8cf5"
 # version "0.9.0"
 
-depends_on "prerelease/prerelease/mulle-match"
+depends_on "mulle-kybernetik/prerelease/mulle-match"
 def install
   system "./bin/installer", "#{prefix}"
 end
