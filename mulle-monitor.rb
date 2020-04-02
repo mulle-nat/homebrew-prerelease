5f795567b4246edac5fbc28bf53f63d019b0ea41
Nat! <nat@codeon.de>
2020-04-02 14:09:06 +0200

    0.9.0 prerelease of mulle-monitor

--- mulle-monitor.rb
+++ mulle-monitor.rb
@@ -5,7 +5,7 @@ url "https://github.com/mulle-sde/mulle-monitor/archive/0.9.0-prerelease.tar.gz"
 sha256 "5a18f89483d94c0a85c20f4c8515316fbf7be152c6c077eb019cdefe1f8c8cf5"
 # version "0.9.0"
 
-depends_on "mulle-kybernetik/prerelease/mulle-match"
+depends_on "mulle-nat/prerelease/mulle-match"
 def install
   system "./bin/installer", "#{prefix}"
 end
