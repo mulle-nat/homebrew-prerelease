133b29368a668c3935cb15a283b13f508c589aa4
Nat! <nat@codeon.de>
2020-04-02 13:41:14 +0200

    2.0.0 prerelease of mulle-dispense

--- mulle-dispense.rb
+++ mulle-dispense.rb
@@ -5,7 +5,7 @@ url "https://github.com/mulle-sde/mulle-dispense/archive/2.0.0-prerelease.tar.gz
 sha256 "d6b9406efb93f0886fde033fa57b8d93a4c0c4b13297977d1005b78c60cf5cd9"
 # version "2.0.0"
 
-depends_on "prerelease/prerelease/mulle-bashfunctions"
+depends_on "mulle-kybernetik/prerelease/mulle-bashfunctions"
 def install
   system "./bin/installer", "#{prefix}"
 end
