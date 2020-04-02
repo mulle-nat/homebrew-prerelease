1206975482b9ace38ca31a0247215fb6733a810c
Nat! <nat@codeon.de>
2020-04-02 14:08:31 +0200

    0.6.0 prerelease of mulle-match

--- mulle-match.rb
+++ mulle-match.rb
@@ -5,7 +5,7 @@ url "https://github.com/mulle-sde/mulle-match/archive/0.6.0-prerelease.tar.gz"
 sha256 "e3cd58842471874e66525c4f12614b5975c766bb75bcaf8e375adccb6d47cb89"
 # version "0.6.0"
 
-depends_on "mulle-kybernetik/prerelease/mulle-bashfunctions"
+depends_on "mulle-nat/prerelease/mulle-bashfunctions"
 def install
   system "./bin/installer", "#{prefix}"
 end
