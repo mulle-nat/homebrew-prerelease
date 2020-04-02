db6ff0695921bcd8ba58a3a43fc03b34262aa51a
Nat! <nat@codeon.de>
2020-04-02 13:43:00 +0200

    1.8.0 prerelease of mulle-fetch

--- mulle-fetch.rb
+++ mulle-fetch.rb
@@ -5,7 +5,7 @@ url "https://github.com/mulle-sde/mulle-fetch/archive/1.8.0-prerelease.tar.gz"
 sha256 "45d4ddb3ec77b23c1d7941e9f7d1b776917eb584369e0308935d7bed4e861701"
 # version "1.8.0"
 
-depends_on "prerelease/prerelease/mulle-bashfunctions"
+depends_on "mulle-kybernetik/prerelease/mulle-bashfunctions"
 def install
   system "./bin/installer", "#{prefix}"
 end
