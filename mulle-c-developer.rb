dec1181b8ad49055a25efafeaa43ca56ca7d5405
Nat! <nat@codeon.de>
2020-04-02 13:57:04 +0200

    0.12.0 prerelease of mulle-c-developer

--- mulle-c-developer.rb
+++ mulle-c-developer.rb
@@ -1,12 +1,12 @@
 class MulleCDeveloper < Formula
 desc "🧢 mulle-c developer kit for mulle-sde"
 homepage "https://github.com/mulle-c/mulle-c-developer"
-url "https://github.com/mulle-c/mulle-c-developer/archive/0.11.0.tar.gz"
-sha256 "a0e65c074fd851e7f4f7fea8703d40eb04280851e71fe47eae388c11d3d35c0b"
-# version "0.11.0"
+url "https://github.com/mulle-c/mulle-c-developer/archive/0.12.0-prerelease.tar.gz"
+sha256 "d1a9257b9564981b05ddb3f13add2fb561b9722b930c5c0e4a337929bdcda87c"
+# version "0.12.0"
 
-depends_on "mulle-kybernetik/software/mulle-sde-developer"
-depends_on "mulle-kybernetik/software/mulle-test"
+depends_on "mulle-kybernetik/prerelease/mulle-sde-developer"
+depends_on "mulle-kybernetik/prerelease/mulle-test"
 def install
   system "./bin/installer", "#{prefix}"
 end
