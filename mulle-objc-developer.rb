1a3c350325110e05bfaf853426e29dc9b81f6177
Nat! <nat@codeon.de>
2020-04-03 12:04:13 +0200

    0.17.0 prerelease of mulle-objc-developer

--- mulle-objc-developer.rb
+++ mulle-objc-developer.rb
@@ -2,7 +2,7 @@ class MulleObjcDeveloper < Formula
 desc "🎩 mulle-objc developer kit for mulle-sde"
 homepage "https://github.com/mulle-objc/mulle-objc-developer"
 url "https://github.com/mulle-objc/mulle-objc-developer/archive/0.17.0-prerelease.tar.gz"
-sha256 "0bbaff9e5983877912f3a5f695177030cbcbee7cb6f97e443267fca3556d431a"
+sha256 "0de6bf6a87716ba8b12b873eadb1250916a2d3407526b8c3f1205cdda2e19eff"
 # version "0.17.0"
 
 depends_on "mulle-nat/prerelease/mulle-c-developer"
