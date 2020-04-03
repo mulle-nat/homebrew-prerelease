307b194448dd7d4449e1687cd33b4f98cd6b070b
Nat! <nat@codeon.de>
2020-04-03 15:15:50 +0200

    0.17.0 prerelease of foundation-developer

--- foundation-developer.rb
+++ foundation-developer.rb
@@ -2,7 +2,7 @@ class FoundationDeveloper < Formula
 desc "👒 Objective C with mulle-sde and the mulle-objc Foundation"
 homepage "https://github.com/MulleFoundation/foundation-developer"
 url "https://github.com/MulleFoundation/foundation-developer/archive/0.17.0-prerelease.tar.gz"
-sha256 "d592dcfb19eb9f35fbe9afeb713214203feed6af1dbfd40c1261368d713656c9"
+sha256 "d0624f92b2969fe80a8dde7fb7d1593ee3f2cd655c11cc9a411f347557d9855a"
 # version "0.17.0"
 
 depends_on "mulle-nat/prerelease/mulle-foundation-developer"
