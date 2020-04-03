f32168635d4de7ff7c7709e467b02b54c5626b36
Nat! <nat@codeon.de>
2020-04-03 16:55:47 +0200

    0.17.0 prerelease of Foundation

--- Foundation.rb
+++ Foundation.rb
@@ -2,7 +2,7 @@ class Foundation < Formula
 desc "💍 Convenient wrapper for the MulleFoundation"
 homepage "https://github.com/MulleFoundation/Foundation"
 url "https://github.com/MulleFoundation/Foundation/archive/0.17.0-prerelease.tar.gz"
-sha256 "6f295f7f403c82c11aec4cad8644e0a5dd12a98033050165e01df2946ec2e7ed"
+sha256 "5570bdc13c371045336ed52992318cb1ca2cf3e3366822cfcf7a63d2ec3b7964"
 # version "0.17.0"
 
 depends_on "codeon-gmbh/software/mulle-clang"
