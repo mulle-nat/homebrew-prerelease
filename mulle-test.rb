19234d0ccb6a9d07d760ceb38d62ed0b7449f5cd
Nat! <nat@mulle-kybernetik.com>
2020-04-06 21:57:59 +0200

    5.2.0 prerelease of mulle-test

--- mulle-test.rb
+++ mulle-test.rb
@@ -1,9 +1,9 @@
 class MulleTest < Formula
 desc "☑︎ Run tests and compare their results against expectations"
 homepage "https://github.com/mulle-sde/mulle-test"
-url "https://github.com/mulle-sde/mulle-test/archive/5.1.0-prerelease.tar.gz"
-sha256 "25a3c957deeb2937f26fb92eda91b0667857a544320627322ea8c3f5738fc95e"
-# version "5.1.0"
+url "https://github.com/mulle-sde/mulle-test/archive/5.2.0-prerelease.tar.gz"
+sha256 "e6a1de884fd7fc0dae5b85e54cd2886f23a72e046bc9ee573d554fff9c760802"
+# version "5.2.0"
 
 depends_on "mulle-nat/prerelease/mulle-platform"
 def install
