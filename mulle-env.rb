f29bffcdd56d0dd9a8c072c54fe636efc6a4f1c2
Nat! <nat@codeon.de>
2020-04-07 11:38:43 +0200

    3.0.0 prerelease of mulle-env

--- mulle-env.rb
+++ mulle-env.rb
@@ -2,7 +2,7 @@ class MulleEnv < Formula
 desc "🌳 Virtual environment for C and Objective-C"
 homepage "https://github.com/mulle-sde/mulle-env"
 url "https://github.com/mulle-sde/mulle-env/archive/3.0.0-prerelease.tar.gz"
-sha256 "c4084efc465e388fb230595b70ad66f8d3521f89b034ef134cb2b389ac1b2e81"
+sha256 "35e948193bb6a117aff93c05b2a0d487e2338bb54677595db7a6f7db503a8977"
 # version "3.0.0"
 
 depends_on "mulle-nat/prerelease/mulle-bashfunctions"
