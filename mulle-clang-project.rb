26916938952d0b198860103e251d54132b571f06
Nat! <nat@codeon.de>
2026-01-16 00:16:34 +0100

    new mulle-clang-project 21.1.8.1

--- mulle-clang-project.rb
+++ mulle-clang-project.rb
@@ -2,24 +2,24 @@ class MulleClangProject < Formula
   desc "Objective-C compiler for the mulle-kybernetik runtime"
   homepage "https://github.com/mulle-cc/mulle-clang-project"
   license "BSD-3-Clause"
-  version "17.0.6.0"
+  version "21.1.8.1"
 #  revision 1
   head "https://github.com/mulle-cc/mulle-clang-project.git", branch: "mulle/17.0.6"
 
 #
 # MEMO:
-#    0. Replace 17.0.6.0 with x.0.0.0 your version number (and check vendor)
+#    0. Replace 21.1.8.1 with x.0.0.0 your version number (and check vendor)
 #    1. Create a release on github
 #    2. Download the tar.gz file from github like so
-#       `curl -O -L "https://github.com/mulle-cc/mulle-clang-project/archive/17.0.6.0.tar.gz"`
-#    3. Run shasum over it `shasum -a 256 -b 17.0.6.0.tar.gz`
+#       `curl -O -L "https://github.com/mulle-cc/mulle-clang-project/archive/21.1.8.1.tar.gz"`
+#    3. Run shasum over it `shasum -a 256 -b 21.1.8.1.tar.gz`
 #    4. Remove bottle urls
 #
-  url "https://github.com/mulle-cc/mulle-clang-project/archive/refs/tags/17.0.6.0.tar.gz"
-  sha256 "abd71ef24387050ca4f22a6f8ea8f27a646b7979a0cc7dc9e4a17f6045fa96d9"
+  url "https://github.com/mulle-cc/mulle-clang-project/archive/refs/tags/21.1.8.1.tar.gz"
+  sha256 "ae7ca7435450b7a81594cafae62a5c23f3a76ce02467146277d23313f4ce950b"
 
   def vendor
-    "mulle-clang 17.0.6.0 (runtime-load-version: 17)"
+    "mulle-clang 21.1.8.1 (runtime-load-version: 18)"
   end
 
 #
@@ -32,19 +32,15 @@ class MulleClangProject < Formula
 #    `brew tap-new mulle-kybernetik/software`
 #    `cp mulle-clang-project.rb /usr/local/Homebrew/Library/Taps/mulle-kybernetik/homebrew-software/Formula/`
 #    `brew bottle mulle-kybernetik/software/mulle-clang-project`
-#    `mv ./mulle-clang--17.0.6.0.sonoma.bottle.tar.gz  ./mulle-clang-project-17.0.6.0.sonoma.bottle.tar.gz`
+#    `mv ./mulle-clang--21.1.8.1.sequoia.bottle.tar.gz  ./mulle-clang-project-21.1.8.1.sequoia.bottle.tar.gz`
 #
 #     scp -i ~/.ssh/id_rsa_hetzner_pw \
-#            ./mulle-clang-17.0.6.0.sonoma.bottle.tar.gz \
+#            ./mulle-clang-21.1.8.1.sequoia.bottle.tar.gz \
 #            codeon@www262.your-server.de:public_html/_site/bottles/
 #
   bottle do
-#    "#{root_url}/#{name}-#{version}.#{tag}.bottle.#{revision}.tar.gz"
-#   root_url "https://www.mulle-kybernetik.com/bottles"
-
-    root_url "https://github.com/mulle-cc/mulle-clang-project/releases/download/17.0.6.0"
-
-    sha256 cellar: :any, sonoma: "aac00f815b7234abf66ac2bb868cc58630f7c2dce325ec351b07d5c5771f267a"
+  root_url "https://github.com/mulle-cc/mulle-clang-project/releases/download/21.1.8.1"
+  sha256 cellar: :any, sequoia: "85b3b7ecd587506db189298e1ca8ae95192f77f8da0d64138834e26f2dbc70f1"
   end
 
 #
@@ -69,8 +65,8 @@ class MulleClangProject < Formula
     mkdir "build" do
       args = std_cmake_args
       args << '-DLLVM_BUILD_LLVM_DYLIB=ON'
-      args << "-DLLVM_ENABLE_PROJECTS='clang'" # ";compiler-rt'" don't build for now on sonoma
-      args << "-DLLVM_ENABLE_RUNTIMES='libcxxabi;libcxx'"
+      args << "-DLLVM_ENABLE_PROJECTS='clang'" # ";compiler-rt'" don't build for now on sequoia
+      args << "-DLLVM_ENABLE_RUNTIMES='libcxxabi;libcxx;libunwind'"
       args << '-DLLVM_LINK_LLVM_DYLIB=ON'
       args << '-DLLVM_PARALLEL_LINK_JOBS=4'
       args << '-DCMAKE_BUILD_TYPE=Release'
